b3ebe766f48cf51f12b3f30759ac8c06727b1d02
iabdalkader <i.abdalkader@gmail.com>
2014-01-23 12:28:11 +0200

    Add Systick Task Scheduling
    * Add periodic task scheduling to systick module.

--- src/systick.c
+++ src/systick.c
@@ -1,16 +1,37 @@
 #include <stdint.h>
 #include <stm32f4xx.h>
 #include <stm32f4xx_misc.h>
+#include <stdlib.h>
 #include "systick.h"
+#include "array.h"
 static volatile uint32_t sys_ticks; 
+static struct array *task_list;
+
+struct systick_task {
+    task_cb cb;
+    uint32_t period;
+};
 
 void SysTick_Handler(void)
 {
+    int i;
+    struct systick_task *task;
+
     ++sys_ticks;       
+
+    for (i=0; i<array_length(task_list); i++) {
+        task = array_at(task_list, i);
+        if ((sys_ticks % task->period)==0) {
+            task->cb();
+        }
+    }
 }
 
 int systick_init()
 {
+    /* Allocate task_list array */
+    array_alloc(&task_list, free);
+
     /* Configure systick to interrupt every 1ms */
     if (SysTick_Config(SystemCoreClock / 1000)) {
         return -1;
@@ -30,4 +51,14 @@ void systick_sleep(uint32_t ms)
 uint32_t systick_current_millis()
 {
     return sys_ticks;
+}
+
+void systick_sched_task(task_cb cb, uint32_t period)
+{
+    struct systick_task *task;
+
+    task = malloc(sizeof(struct systick_task));
+    task->cb = cb;
+    task->period = period;
+    array_push_back(task_list, task);
 }
--- src/systick.h
+++ src/systick.h
@@ -1,6 +1,8 @@
 #ifndef __SYSTICK_H__
 #define __SYSTICK_H__
+typedef void (*task_cb) ();
 int systick_init();
 void systick_sleep(uint32_t ms);
 uint32_t systick_current_millis();
+void systick_sched_task(task_cb cb, uint32_t period);
 #endif /* __SYSTICK_H__ */
