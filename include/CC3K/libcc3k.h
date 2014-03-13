669a9effb46a1329e632d172621291cb835876ec
iabdalkader <i.abdalkader@gmail.com>
2014-03-13 02:10:18 +0200

    Add CC3000 library

---
+++ include/CC3K/libcc3k.h
@@ -0,0 +1,11 @@
+#ifndef __LIBCC3K_H__
+#define __LIBCC3K_H__
+#include "hci.h"
+#include "spi.h"
+#include "wlan.h"
+#include "socket.h"
+#include "nvmem.h"
+#include "netapp.h"
+#endif //__LIBCC3K_H__
+
+
