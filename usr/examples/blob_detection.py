df5808b27dca8ee021d5c173b2134685cdb42d79
iabdalkader <i.abdalkader@gmail.com>
2014-03-22 19:45:03 +0200

    Fix blob example

--- usr/examples/blob_detection.py
+++ usr/examples/blob_detection.py
@@ -1,15 +1,20 @@
 import sensor, imlib, time
+sensor.set_pixformat(sensor.RGB565)
 clock = time.clock()
 while (True):
+    clock.tick()
     # take snapshot
     image = sensor.snapshot()
 
     # detect blobs
-    imlib.threshold(image, (255, 127, 127),  48)
-    clock.tick()
+    imlib.threshold(image, (255, 127, 127),  40)
     imlib.median(image, 3)
     blobs = imlib.count_blobs(image)
-    image = sensor.snapshot()
+    
+    # draw rectangles around detected blobs
+    image = sensor.snapshot()    
     for r in blobs:
-        imlib.draw_rectangle(image, r)
+        imlib.draw_rectangle(image, r)        
+        
+    print(clock.fps())
     break
