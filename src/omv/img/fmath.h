44669573cee0576ecc3088096c1c7e9cf2747ffd
iabdalkader <i.abdalkader@gmail.com>
2014-09-11 05:52:43 +0200

    Minor fixes

--- src/omv/img/fmath.h
+++ src/omv/img/fmath.h
@@ -6,7 +6,7 @@ int fast_floorf(float x);
 int fast_ceilf(float x);
 int fast_roundf(float x);
 float fast_atanf(float x);
-float fast_atan2f(float x, float y);
+float fast_atan2f(float y, float x);
 float fast_expf(float x);
 float fast_cbrtf(float d);
 float fast_fabsf(float d);
