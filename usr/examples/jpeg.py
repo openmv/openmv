156c349e4375b066561b147228b035f3308ec0dd
iabdalkader <i.abdalkader@gmail.com>
2014-09-17 22:19:07 +0200

    Add JPEG Example

---
+++ usr/examples/jpeg.py
@@ -0,0 +1,19 @@
+import sensor, time, led
+sensor.reset()
+sensor.set_contrast(2)
+sensor.set_framesize(sensor.QQVGA)
+sensor.set_pixformat(sensor.RGB565)
+
+clock = time.clock()
+# Take snapshot
+image = sensor.snapshot()
+
+# Compress Image
+clock.tick()
+img = image.compress(30)
+print(clock.avg())
+
+f = open("/test.jpeg", "w")
+f.write(img)
+f.close()
+break
