b5d6de47ee483171ca53bab2bd09ad754fe3fe4f
iabdalkader <i.abdalkader@gmail.com>
2014-09-20 02:31:14 +0200

    Update FB Viewer

--- usr/examples/fb_viewer.py
+++ usr/examples/fb_viewer.py
@@ -1,20 +1,18 @@
 import sensor, time
-
 sensor.reset()
-
 # Set sensor parameters
-sensor.set_brightness(-2)
 sensor.set_contrast(1)
-#sensor.set_gainceiling(5)
+sensor.set_gainceiling(8)
 
 # Set sensor pixel format
 sensor.set_framesize(sensor.QVGA)
 sensor.set_pixformat(sensor.JPEG)
-sensor.set_quality(98)
+sensor.set_quality(95)
 
 clock = time.clock()
 
 while (True):
   clock.tick()
   image = sensor.snapshot()
+  time.sleep(1)
   print(clock.fps())
