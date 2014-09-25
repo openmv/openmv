b9b1eee93e9e5d278909ed6f9f43832efdb66f6d
iabdalkader <i.abdalkader@gmail.com>
2014-09-25 19:12:59 +0200

    Cleanup open/new/save/save_as

--- usr/openmv-ide.py
+++ usr/openmv-ide.py
@@ -84,6 +84,7 @@ class OMVGtk:
             "on_button_press"               : self.button_pressed,
             "on_button_release"             : self.button_released,
             "on_open_file"                  : self.open_file,
+            "on_new_file"                   : self.new_file,
             "on_save_file"                  : self.save_file,
             "on_save_file_as"               : self.save_file_as,
             "on_save_template_activate"     : self.save_template,
@@ -107,15 +108,12 @@ class OMVGtk:
         self.builder.get_object('refresh_button').set_sensitive(False)
 
         # open last opened file
+        self.file_path= None
         if os.path.isfile(CONFIG_PATH):
             with open(CONFIG_PATH, "r") as file:
-                self.file_path = file.read()
-            if os.path.isfile(self.file_path):
-                with open(self.file_path, "r") as file:
-                    self.buffer.set_text(file.read())
-                    self.window.set_title(os.path.basename(self.file_path))
-            else:
-                self.file_path = None
+                path = file.read()
+            if os.path.isfile(path):
+                self._load_file(path)
 
         # build examples menu
         if os.path.isdir(EXAMPLE_PATH):
@@ -291,31 +289,41 @@ class OMVGtk:
     def on_ctrl_scale_value_changed(self, adjust):
         openmv.set_attr(adjust.attr, int(adjust.value))
 
-    def open_file(self, widget):
-        dialog = gtk.FileChooserDialog(title=None,action=gtk.FILE_CHOOSER_ACTION_OPEN,
-                buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
-        dialog.set_default_response(gtk.RESPONSE_OK)
-        dialog.set_current_folder("./examples/")
-        ff = gtk.FileFilter()
-        ff.set_name("python")
-        ff.add_pattern("*.py")
-        dialog.add_filter(ff)
-
-        if dialog.run() == gtk.RESPONSE_OK:
-            with open(dialog.get_filename(), "r") as file:
-                self.buffer.set_text(file.read())
-                self.file_path=dialog.get_filename()
-                self.save_button.set_sensitive(False)
-                self.window.set_title(os.path.basename(self.file_path))
-
-        dialog.destroy()
-
-    def open_example(self, widget):
-        self.file_path = os.path.join(EXAMPLE_PATH, widget.get_label())
-        with open(self.file_path, "r") as file:
-            self.buffer.set_text(file.read())
+    def _load_file(self, path):
+        self.file_path = path
+        if path == None: # New file
+            self.save_button.set_sensitive(True)
+            self.buffer.set_text("")
+            self.window.set_title("Untitled")
+        else:
+            self.save_button.set_sensitive(False)
             self.window.set_title(os.path.basename(self.file_path))
+            with open(path, "r") as file:
+                self.buffer.set_text(file.read())
 
+    def _save_file(self, new_file):
+        if new_file:
+            dialog = gtk.FileChooserDialog(title=None,action=gtk.FILE_CHOOSER_ACTION_SAVE,
+                    buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
+            dialog.set_default_response(gtk.RESPONSE_OK)
+            dialog.set_current_folder(EXAMPLE_PATH)
+            ff = gtk.FileFilter()
+            ff.set_name("python")
+            ff.add_pattern("*.py")
+            dialog.add_filter(ff)
+
+            if dialog.run() == gtk.RESPONSE_OK:
+                self.file_path = dialog.get_filename()
+                self.save_button.set_sensitive(False)
+                self.window.set_title(os.path.basename(dialog.get_filename()))
+                with open(dialog.get_filename(), "w") as file:
+                    file.write(self.buffer.get_text(self.buffer.get_start_iter(), self.buffer.get_end_iter()))
+
+            dialog.destroy()
+        else:
+            self.save_button.set_sensitive(False)
+            with open(self.file_path, "w") as file:
+                file.write(self.buffer.get_text(self.buffer.get_start_iter(), self.buffer.get_end_iter()))
 
     def save_template(self, widget):
         self.da_menu.hide()
@@ -356,26 +364,32 @@ class OMVGtk:
             openmv.save_descriptor(x/SCALE, y/SCALE, w/SCALE, h/SCALE, entry.get_text()) #Use Scale
         dialog.hide()
 
+    def new_file(self, widget):
+        self._load_file(None)
+
     def save_file(self, widget):
-        self.save_button.set_sensitive(False)
-        with open(self.file_path, "w") as file:
-            file.write(self.buffer.get_text(self.buffer.get_start_iter(), self.buffer.get_end_iter()))
+        self._save_file(self.file_path==None)
 
     def save_file_as(self, widget):
-        dialog = gtk.FileChooserDialog(title=None,action=gtk.FILE_CHOOSER_ACTION_SAVE,
+        self._save_file(True)
+
+    def open_file(self, widget):
+        dialog = gtk.FileChooserDialog(title=None,action=gtk.FILE_CHOOSER_ACTION_OPEN,
                 buttons=(gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL, gtk.STOCK_OPEN, gtk.RESPONSE_OK))
         dialog.set_default_response(gtk.RESPONSE_OK)
-        dialog.set_current_folder("./examples/")
+        dialog.set_current_folder(EXAMPLE_PATH)
         ff = gtk.FileFilter()
         ff.set_name("python")
         ff.add_pattern("*.py")
         dialog.add_filter(ff)
 
         if dialog.run() == gtk.RESPONSE_OK:
-            with open(dialog.get_filename(), "w") as file:
-                file.write(self.buffer.get_text(self.buffer.get_start_iter(), self.buffer.get_end_iter()))
+            self._load_file(dialog.get_filename())
 
         dialog.destroy()
+
+    def open_example(self, widget):
+        self._load_file(os.path.join(EXAMPLE_PATH, widget.get_label()))
 
     def text_changed(self, widget):
         self.save_button.set_sensitive(True)
@@ -384,9 +398,10 @@ class OMVGtk:
         # disconnect
         self.disconnect()
 
-        # write last opened file
-        with open(CONFIG_PATH, "w") as file:
-            file.write(self.file_path)
+        if (self.file_path):
+            # write last opened file
+            with open(CONFIG_PATH, "w") as file:
+               file.write(self.file_path)
 
         # exit
         sys.exit(0)
