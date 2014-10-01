6fd70bb73460a4e0c4f731d14b2f363be19cffbe
iabdalkader <i.abdalkader@gmail.com>
2014-10-01 02:03:55 +0200

    Update example

--- usr/examples/face_detection.py
+++ usr/examples/face_detection.py
@@ -1,19 +1,24 @@
 import sensor, time
+sensor.reset()
+# Set sensor brightness
+sensor.set_contrast(1)
 # Set sensor gainceiling
 sensor.set_gainceiling(16)
-# Set sensor brightness
-sensor.set_brightness(-2)
+# Set framesize
+sensor.set_framesize(sensor.QCIF)
 # Set sensor to grayscale
 sensor.set_pixformat(sensor.GRAYSCALE)
 
 # Load Haar Cascade
-cascade = HaarCascade("0:/frontalface_default.cascade")
-print(cascade)
+face_cascade = HaarCascade("/frontalface.cascade")
+print(face_cascade)
 clock = time.clock()
 while (True):
     clock.tick()
     image = sensor.snapshot()
-    objects = image.find_features(cascade)
+    objects = image.find_features(face_cascade, threshold=0.65, scale=1.65)
     for r in objects:
         image.draw_rectangle(r)
+    #Add delay to see drawing on FB        
+    time.sleep(10)
     print (clock.fps())
