874e4b99bb37f92763b51d4e6da96d25595cb60c
iabdalkader <i.abdalkader@gmail.com>
2014-11-03 17:00:28 +0200

    Open serial port after calling openmv.init
    * Handle the case where init changes the configuration

--- usr/openmv-ide.py
+++ usr/openmv-ide.py
@@ -193,20 +193,20 @@ class OMVGtk:
 
     def connect(self):
         try:
-            # open VCP and configure the terminal
-            self.serial = serial.Serial(self.config.get("main", "serial_port"), 115200, timeout=0.001)
-            gobject.gobject.idle_add(omvgtk.update_terminal)
-        except Exception as e:
-            self.show_message_dialog(gtk.MESSAGE_ERROR, "Failed to connect to OpenMV\n%s"%e)
-            return
-
-        try:
             # init openmv
             openmv.init()
 
             # interrupt any running code
             openmv.stop_script()
             sleep(0.1)
+        except Exception as e:
+            self.show_message_dialog(gtk.MESSAGE_ERROR, "Failed to connect to OpenMV\n%s"%e)
+            return
+
+        try:
+            # open VCP and configure the terminal
+            self.serial = serial.Serial(self.config.get("main", "serial_port"), 115200, timeout=0.001)
+            gobject.gobject.idle_add(omvgtk.update_terminal)
         except Exception as e:
             self.show_message_dialog(gtk.MESSAGE_ERROR, "Failed to connect to OpenMV\n%s"%e)
             return
