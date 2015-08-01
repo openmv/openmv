939c47da6c64a020771358c31e5ccefb610348a3
iabdalkader <i.abdalkader@gmail.com>
2015-08-01 11:06:54 +0200

    Add ring buffer

---
+++ src/omv/ringbuf.c
@@ -0,0 +1,45 @@
+/*
+ * This file is part of the OpenMV project.
+ * Copyright (c) 2013/2014 Ibrahim Abdelkader <i.abdalkader@gmail.com>
+ * This work is licensed under the MIT license, see the file LICENSE for details.
+ *
+ * Simple Ring Buffer implementation.
+ *
+ */
+#include <string.h>
+#include <stdint.h>
+#include "ringbuf.h"
+
+void ring_buf_init(ring_buf_t *buf)
+{
+    memset(buf, 0, sizeof(*buf));
+}
+
+int ring_buf_empty(ring_buf_t *buf)
+{
+    return (buf->head == buf->tail);
+}
+
+void ring_buf_put(ring_buf_t *buf, uint8_t c)
+{
+   if ((buf->tail + 1) % BUFFER_SIZE == buf->head) {
+      /*buffer is full*/
+      return;
+   }
+   
+   buf->data[buf->tail] = c;
+   buf->tail = (buf->tail + 1) % BUFFER_SIZE;   
+}
+
+uint8_t ring_buf_get(ring_buf_t *buf)
+{
+   uint8_t c;
+   if (buf->head == buf->tail) {
+       /*buffer is empty*/
+       return 0;
+   }
+   
+   c = buf->data[buf->head];
+   buf->head = (buf->head + 1) % BUFFER_SIZE;
+   return c;
+}
