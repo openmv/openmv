402159801ba7091d4a06653496e90fca1cd31183
iabdalkader <i.abdalkader@gmail.com>
2015-08-19 01:04:17 +0200

    Zip PyInstaller packages

--- util/mkpkg.sh
+++ util/mkpkg.sh
@@ -4,10 +4,10 @@
 DIST_DIR=dist
 BUILD_DIR=build
 OPENMV_DIR=openmv
-PACKAGE=$(python -c "import sys,platform; print('openmv_'+sys.platform+'_'+platform.machine()+'.tar.gz'.lower())")
+PACKAGE=$(python -c "import sys,platform; print('openmv_'+sys.platform+'_'+platform.machine()+'.zip'.lower())")
 SPEC_FILE=openmv-ide.spec
 
 rm -fr $DIST_DIR $BUILD_DIR $PACKAGE
 pyinstaller $SPEC_FILE
-(cd $DIST_DIR && tar -cvzf ../$PACKAGE $OPENMV_DIR)
+(cd $DIST_DIR && zip ../$PACKAGE -r $OPENMV_DIR)
 rm -fr $DIST_DIR $BUILD_DIR
