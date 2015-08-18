b7b9cfd64bee9284f7d815d203654c65d68e9cff
iabdalkader <i.abdalkader@gmail.com>
2015-08-19 00:51:23 +0200

    Fix pyinstaller package name

--- util/mkpkg.sh
+++ util/mkpkg.sh
@@ -4,7 +4,7 @@
 DIST_DIR=dist
 BUILD_DIR=build
 OPENMV_DIR=openmv
-PACKAGE=$(python -c "import sys,platform; print(''.join(sys.platform+'_'+platform.machine()).lower())")
+PACKAGE=$(python -c "import sys,platform; print('openmv_'+sys.platform+'_'+platform.machine()+'.tar.gz'.lower())")
 SPEC_FILE=openmv-ide.spec
 
 rm -fr $DIST_DIR $BUILD_DIR $PACKAGE
