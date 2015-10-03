fc2a5bb1c77b655bfb6098e111016bb97de0bc9c
iabdalkader <i.abdalkader@gmail.com>
2015-10-03 23:53:03 +0200

    Use python2 to run openmv.py, pydfu.py and openmv-ide.py

--- usr/openmv.py
+++ usr/openmv.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # This file is part of the OpenMV project.
 # Copyright (c) 2013/2014 Ibrahim Abdelkader <i.abdalkader@gmail.com>
 # This work is licensed under the MIT license, see the file LICENSE for details.
