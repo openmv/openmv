095c95a998160feb0d432b529268148410a10f85
Kwabena W. Agyeman <kwagyeman@live.com>
2016-02-12 00:31:26 -0500

    Made Soft I2C Pretty

--- src/omv/soft_i2c.h
+++ src/omv/soft_i2c.h
@@ -9,7 +9,7 @@
 #ifndef __SOFT_I2C_H__
 #define __SOFT_I2C_H__
 #include <stdint.h>
-void soft_i2c_init();
 int soft_i2c_read_bytes(uint8_t slv_addr, uint8_t *buf, int len, bool stop);
 int soft_i2c_write_bytes(uint8_t slv_addr, uint8_t *buf, int len, bool stop);
+void soft_i2c_init();
 #endif // __SOFT_I2C_H__
