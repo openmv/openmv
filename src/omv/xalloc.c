456c4ab7574b87b8b7d2f64da60dfc242e655fdf
Kwabena W. Agyeman <kwagyeman@live.com>
2016-02-13 13:56:33 -0500

    Made XALLOC better. Now the functions detect if you are trying to alloc 0 bytes and don't fail if you do that. Additionally, I added some comments on behavior. (I studied what the gc functions did extensively to know the behavior of this stuff). All changes have been tested with code that does memory allocs.

--- src/omv/xalloc.c
+++ src/omv/xalloc.c
@@ -7,45 +7,48 @@
  *
  */
 #include <mp.h>
-#include "mdefs.h"
 #include "xalloc.h"
 
-void *xalloc_fail()
+NORETURN static void xalloc_fail()
 {
-    nlr_raise(mp_obj_new_exception_msg(&mp_type_MemoryError, "Out of Memory!!"));
-    return NULL;
+    nlr_raise(mp_obj_new_exception_msg(&mp_type_MemoryError, "Out of Memory!!!"));
 }
 
+// returns null pointer without error if size==0
 void *xalloc(uint32_t size)
 {
     void *mem = gc_alloc(size, false);
-    if (mem == NULL) {
-        return xalloc_fail();
+    if (size && (mem == NULL)) {
+        xalloc_fail();
     }
     return mem;
 }
 
+// returns null pointer without error if size==0
 void *xalloc0(uint32_t size)
 {
     void *mem = gc_alloc(size, false);
-    if (mem == NULL) {
-        return xalloc_fail();
+    if (size && (mem == NULL)) {
+        xalloc_fail();
     }
     memset(mem, 0, size);
     return mem;
 }
 
+// returns without error if mem==null
 void xfree(void *mem)
 {
     gc_free(mem);
 }
 
+// returns null pointer without error if size==0
+// allocs if mem==null and size!=0
+// frees if mem!=null and size==0
 void *xrealloc(void *mem, uint32_t size)
 {
     mem = gc_realloc(mem, size);
-    if (mem == NULL) {
-        return xalloc_fail();
+    if (size && (mem == NULL)) {
+        xalloc_fail();
     }
     return mem;
 }
-
--- src/omv/xalloc.h
+++ src/omv/xalloc.h
@@ -9,9 +9,8 @@
 #ifndef __XALLOC_H__
 #define __XALLOC_H__
 #include <stdint.h>
-void xalloc_init();
 void *xalloc(uint32_t size);
 void *xalloc0(uint32_t size);
-void xfree(void *ptr);
-void *xrealloc(void *ptr, uint32_t size);
+void xfree(void *mem);
+void *xrealloc(void *mem, uint32_t size);
 #endif // __XALLOC_H__
