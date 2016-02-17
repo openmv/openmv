dc8678057893164779fad2bc3a46ef87a719e645
iabdalkader <i.abdalkader@gmail.com>
2016-02-17 20:32:50 +0200

    Use FB_PIXELS() to get a pointer to fb pixels.

--- src/omv/fb_alloc.c
+++ src/omv/fb_alloc.c
@@ -7,15 +7,15 @@
  *
  */
 #include <mp.h>
-#include "framebuffer.h"
 #include "fb_alloc.h"
+#include "framebuffer.h"
 
 extern char _fs_cache;
 static char *pointer = &_fs_cache;
 
 NORETURN static void fb_alloc_fail()
 {
-    nlr_raise(mp_obj_new_exception_msg(&mp_type_MemoryError, "Out of Memory!!!"));
+    nlr_raise(mp_obj_new_exception_msg(&mp_type_MemoryError, "FB Alloc Collision!!!"));
 }
 
 void fb_alloc_init0()
@@ -32,15 +32,12 @@ void *fb_alloc(uint32_t size)
     size=((size+sizeof(uint32_t)-1)/sizeof(uint32_t))*sizeof(uint32_t);// Round Up
     char *result = pointer - size;
     char *new_pointer = result - sizeof(uint32_t);
-    char *end_pointer = (char *)fb->pixels; // NULL (fb->bpp <= 0)
-    if ((fb->bpp == 1) || (fb->bpp == 2)) { // GRAYSCALE OR RGB565
-        end_pointer += fb->w * fb->w * fb->bpp;
-    } else if (fb->bpp >= 3) { // JPEG
-        end_pointer += fb->bpp;
-    }
-    if (new_pointer < end_pointer) {
+
+    // Check if allocation overwrites the framebuffer pixels
+    if (new_pointer < (char *) FB_PIXELS()) {
         fb_alloc_fail();
     }
+
     *((uint32_t *) new_pointer) = size + sizeof(uint32_t); // Save size.
     pointer = new_pointer;
     return result;
