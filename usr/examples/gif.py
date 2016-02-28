052d64e79cb2604b323e6d816f221ff268910be3
iabdalkader <i.abdalkader@gmail.com>
2016-02-28 21:37:23 +0200

    Set frame size to QQVGA in gif script.

--- usr/examples/gif.py
+++ usr/examples/gif.py
@@ -8,7 +8,7 @@ sensor.set_contrast(1)
 sensor.set_brightness(1)
 sensor.set_saturation(1)
 sensor.set_gainceiling(16)
-sensor.set_framesize(sensor.QVGA)
+sensor.set_framesize(sensor.QQVGA)
 sensor.set_pixformat(sensor.GRAYSCALE)
 
 for i in range(30):
