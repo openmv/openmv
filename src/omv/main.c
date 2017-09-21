07b7fb1f21eddb4e0c3d800c9d8d7b6bce012b67
iabdalkader <i.abdalkader@gmail.com>
2017-09-21 16:41:19 +0200

    Update main.c for MP 1.9.

--- src/omv/main.c
+++ src/omv/main.c
@@ -34,10 +34,11 @@
 #include "sdcard.h"
 #include "ff.h"
 #include "modnetwork.h"
+#include "modmachine.h"
 
+#include "extmod/vfs.h"
+#include "extmod/vfs_fat.h"
 #include "lib/utils/pyexec.h"
-#include "lib/fatfs/ff.h"
-#include "extmod/fsusermount.h"
 
 #include "irq.h"
 #include "rng.h"
@@ -70,9 +71,10 @@
 #include "framebuffer.h"
 
 int errno;
-extern char _vfs_buf;
-extern char _stack_size;
-static fs_user_mount_t *vfs = (fs_user_mount_t *) &_vfs_buf;
+// Not sure why MP doesn't like vfs stored here anymore.
+//extern char _vfs_buf;
+//static fs_user_mount_t *vfs_fat = (fs_user_mount_t *) &_vfs_buf;
+static fs_user_mount_t _vfs_fat, *vfs_fat = &_vfs_fat;
 
 static const char fresh_main_py[] =
 "# main.py -- put your code here!\n"
@@ -196,9 +198,9 @@ void flash_error(int n) {
 
 void NORETURN __fatal_error(const char *msg) {
     FIL fp;
-    if (f_open(&fp, "ERROR.LOG",
+    if (f_open(&vfs_fat->fatfs, &fp, "ERROR.LOG",
                FA_WRITE|FA_CREATE_ALWAYS) == FR_OK) {
-        f_printf(&fp, "\nFATAL ERROR:\n%s\n", msg);
+        //f_printf(&fp, "\nFATAL ERROR:\n%s\n", msg);
     }
     f_close(&fp);
     storage_flush();
@@ -245,29 +247,30 @@ STATIC mp_obj_t pyb_main(mp_obj_t main) {
 
 MP_DEFINE_CONST_FUN_OBJ_1(pyb_main_obj, pyb_main);
 
-static void make_flash_fs()
+void make_flash_fs()
 {
     FIL fp;
     UINT n;
 
     led_state(LED_RED, 1);
 
-    if (f_mkfs("0:", 0, 0) != FR_OK) {
-        __fatal_error("could not create LFS");
+    uint8_t working_buf[_MAX_SS];
+    if (f_mkfs(&vfs_fat->fatfs, FM_FAT, 0, working_buf, sizeof(working_buf)) != FR_OK) {
+        __fatal_error("Could not create LFS");
     }
 
-    // create default main.py
-    f_open(&fp, "main.py", FA_WRITE | FA_CREATE_ALWAYS);
+    // Create default main.py
+    f_open(&vfs_fat->fatfs, &fp, "/main.py", FA_WRITE | FA_CREATE_ALWAYS);
     f_write(&fp, fresh_main_py, sizeof(fresh_main_py) - 1 /* don't count null terminator */, &n);
     f_close(&fp);
 
-    // create readme file
-    f_open(&fp, "README.txt", FA_WRITE | FA_CREATE_ALWAYS);
+    // Create readme file
+    f_open(&vfs_fat->fatfs, &fp, "/README.txt", FA_WRITE | FA_CREATE_ALWAYS);
     f_write(&fp, fresh_readme_txt, sizeof(fresh_readme_txt) - 1 /* don't count null terminator */, &n);
     f_close(&fp);
 
-    // create default selftest.py
-    f_open(&fp, "selftest.py", FA_WRITE | FA_CREATE_ALWAYS);
+    // Create default selftest.py
+    f_open(&vfs_fat->fatfs, &fp, "/selftest.py", FA_WRITE | FA_CREATE_ALWAYS);
     f_write(&fp, fresh_selftest_py, sizeof(fresh_selftest_py) - 1 /* don't count null terminator */, &n);
     f_close(&fp);
 
@@ -299,15 +302,15 @@ int main(void)
     //  - Set NVIC Group Priority to 4
     //  - Configure the Flash prefetch, instruction and Data caches
     //  - Configure the Systick to generate an interrupt each 1 msec
-    //  Note: The bootloader enables the CCM/DTCM memory.
+    //  NOTE: The bootloader enables the CCM/DTCM memory.
     HAL_Init();
 
     // Stack limit should be less than real stack size, so we have a chance
     // to recover from limit hit.  (Limit is measured in bytes.)
-    mp_stack_ctrl_init();
+    mp_stack_set_top(&_ram_end);
     mp_stack_set_limit((char*)&_ram_end - (char*)&_heap_end - 1024);
 
-    // basic sub-system init
+    // Basic sub-system init
     led_init();
     pendsv_init();
 
@@ -320,6 +323,8 @@ soft_reset:
     led_state(LED_GREEN, 1);
     led_state(LED_BLUE, 1);
 
+    machine_init();
+
     // GC init
     gc_init(&_heap_start, &_heap_end);
 
@@ -328,13 +333,8 @@ soft_reset:
     mp_obj_list_init(mp_sys_path, 0);
     mp_obj_list_init(mp_sys_argv, 0);
 
-    // zero out the pointers to the mounted devices
-    memset(MP_STATE_PORT(fs_user_mount), 0, sizeof(MP_STATE_PORT(fs_user_mount)));
-
-    // Initialise low-level sub-systems.  Here we need to very basic things like
-    // zeroing out memory and resetting any of the sub-systems.  Following this
-    // we can run Python scripts (eg main.py).
-
+    // Initialise low-level sub-systems. Here we need to do the very basic
+    // things like zeroing out memory and resetting any of the sub-systems.
     readline_init0();
     pin_init0();
     extint_init0();
@@ -351,12 +351,15 @@ soft_reset:
     file_buffer_init0();
     py_lcd_init0();
     py_fir_init0();
+    servo_init();
+    usbdbg_init();
 
-#if MICROPY_HW_ENABLE_RTC
+    //TODO
+    //mod_network_init();
+
     if (first_soft_reset) {
         rtc_init_start(false);
     }
-#endif
 
     // Initialize the sensor and check the result after
     // mounting the file-system to log errors (if any).
@@ -364,69 +367,62 @@ soft_reset:
         sensor_init_ret = sensor_init();
     }
 
-    servo_init();
-    usbdbg_init();
-    mod_network_init();
-
     // Remove the BASEPRI masking (if any)
     irq_set_base_priority(0);
 
     // Initialize storage
     if (sdcard_is_present()) {
         sdcard_init();
-        // init the vfs object
-        vfs->str = "1:";
-        vfs->len = 2;
-        vfs->flags = 0;
-        sdcard_init_vfs(vfs);
 
-        // put the sdcard device in slot 1 (it will be unused at this point)
-        MP_STATE_PORT(fs_user_mount)[1] = vfs;
+        // Init the vfs object
+        vfs_fat->flags = 0;
+        sdcard_init_vfs(vfs_fat, 1);
 
-        // try to mount the flash
-        FRESULT res = f_mount(&vfs->fatfs, vfs->str, 1);
+        // Try to mount the SD card 
+        FRESULT res = f_mount(&vfs_fat->fatfs);
         if (res != FR_OK) {
-            __fatal_error("could not mount SD\n");
+            __fatal_error("Could not mount SD card\n");
         }
-        // Set CWD and USB medium to SD
-        f_chdrive("1:");
+
+        // Set USB medium to SD
         pyb_usb_storage_medium = PYB_USB_STORAGE_MEDIUM_SDCARD;
     } else {
         storage_init();
 
         // init the vfs object
-        vfs->str = "0:";
-        vfs->len = 2;
-        vfs->flags = 0;
-        pyb_flash_init_vfs(vfs);
+        vfs_fat->flags = 0;
+        pyb_flash_init_vfs(vfs_fat);
 
-        // put the flash device in slot 0 (it will be unused at this point)
-        MP_STATE_PORT(fs_user_mount)[0] = vfs;
-
-        // try to mount the flash
-        FRESULT res = f_mount(&vfs->fatfs, vfs->str, 1);
+        // Try to mount the flash
+        FRESULT res = f_mount(&vfs_fat->fatfs);
 
         if (res == FR_NO_FILESYSTEM) {
-            // create a fresh fs
+            // Create a fresh fs
             make_flash_fs();
         } else if (res != FR_OK) {
-            __fatal_error("could not access LFS\n");
+            __fatal_error("Could not access LFS\n");
         }
 
-        // Set CWD and USB medium to flash
-        f_chdrive("0:");
+        // Set USB medium to flash
         pyb_usb_storage_medium = PYB_USB_STORAGE_MEDIUM_FLASH;
     }
 
-    // turn boot-up LEDs off
-    led_state(LED_RED, 0);
-    led_state(LED_GREEN, 0);
-    led_state(LED_BLUE, 0);
-
-    // init USB device to default setting if it was not already configured
-    if (!(pyb_usb_flags & PYB_USB_FLAG_USB_MODE_CALLED)) {
-        pyb_usb_dev_init(USBD_VID, USBD_PID_CDC_MSC, USBD_MODE_CDC_MSC, NULL);
+    // Mount the storage device (there should be no other devices mounted at this point)
+    // we allocate this structure on the heap because vfs->next is a root pointer.
+    mp_vfs_mount_t *vfs = m_new_obj_maybe(mp_vfs_mount_t);
+    if (vfs == NULL) {
+        // TODO
     }
+
+    vfs->str = "/";
+    vfs->len = 1;
+    vfs->obj = MP_OBJ_FROM_PTR(vfs_fat);
+    vfs->next = NULL;
+    MP_STATE_VM(vfs_mount_table) = vfs;
+
+    // The current directory is used as the boot up directory.
+    // It is set to the internal flash filesystem by default.
+    MP_STATE_PORT(vfs_cur) = vfs;
 
     // check sensor init result
     if (first_soft_reset && sensor_init_ret != 0) {
@@ -435,30 +431,42 @@ soft_reset:
         __fatal_error(buf);
     }
 
-    // Run self tests the first time only
-    f_res = f_stat("selftest.py", NULL);
-    if (first_soft_reset && f_res == FR_OK) {
-        nlr_buf_t nlr;
-        if (nlr_push(&nlr) == 0) {
-            // Parse, compile and execute the self-tests script.
-            pyexec_file("selftest.py");
-            nlr_pop();
-        } else {
-            // Get the exception message. TODO: might be a hack.
-            mp_obj_str_t *str = mp_obj_exception_get_value((mp_obj_t)nlr.ret_val);
-            // If any of the self-tests fail log the exception message
-            // and loop forever. Note: IDE exceptions will not be caught.
-            __fatal_error((const char*) str->data);
-        }
-        // Success: remove self tests script and flush cache
-        f_unlink("selftest.py");
-        storage_flush();
-        // Set flag for SWD debugger (main.py does not use the frame buffer).
-        MAIN_FB()->bpp = 0xDEADBEEF;
+    // Turn boot-up LEDs off
+    led_state(LED_RED, 0);
+    led_state(LED_GREEN, 0);
+    led_state(LED_BLUE, 0);
+
+    // Init USB device to default setting if it was not already configured
+    if (!(pyb_usb_flags & PYB_USB_FLAG_USB_MODE_CALLED)) {
+        pyb_usb_dev_init(USBD_VID, USBD_PID_CDC_MSC, USBD_MODE_CDC_MSC, NULL);
     }
 
-    // Run the main script from the current directory.
-    f_res = f_stat("main.py", NULL);
+    // Run self tests the first time only
+    //f_res = f_stat_helper("/selftest.py", NULL);
+    //if (first_soft_reset && f_res == FR_OK) {
+    //    nlr_buf_t nlr;
+    //    if (nlr_push(&nlr) == 0) {
+    //        // Parse, compile and execute the self-tests script.
+    //        pyexec_file("/selftest.py");
+    //        nlr_pop();
+    //    } else {
+    //        // Get the exception message. TODO: might be a hack.
+    //        mp_obj_str_t *str = mp_obj_exception_get_value((mp_obj_t)nlr.ret_val);
+    //        // If any of the self-tests fail log the exception message
+    //        // and loop forever. Note: IDE exceptions will not be caught.
+    //        __fatal_error((const char*) str->data);
+    //    }
+
+    //    // Success: remove self tests script and flush cache
+    //    f_unlink_helper("/selftest.py");
+    //    storage_flush();
+
+    //    // Set flag for SWD debugger (main.py does not use the frame buffer).
+    //    MAIN_FB()->bpp = 0xDEADBEEF;
+    //}
+
+    // Run the main script
+    f_res = f_stat_helper("/main.py", NULL);
     if (first_soft_reset && f_res == FR_OK) {
         nlr_buf_t nlr;
         if (nlr_push(&nlr) == 0) {
@@ -468,7 +476,7 @@ soft_reset:
             usbdbg_set_script_running(true);
 
             // Parse, compile and execute the main script.
-            pyexec_file("main.py");
+            pyexec_file("/main.py");
             nlr_pop();
         } else {
             // Disable IDE interrupt and clear script running
