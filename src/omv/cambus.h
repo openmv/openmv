ff8cf1616990d23c9c81d89a45371891b398fc11
iabdalkader <i.abdalkader@gmail.com>
2017-10-18 23:19:12 +0200

    Rename data to reg_data.

--- src/omv/cambus.h
+++ src/omv/cambus.h
@@ -11,8 +11,8 @@
 #include <stdint.h>
 int cambus_init();
 int cambus_scan();
-int cambus_readb(uint8_t slv_addr, uint8_t reg_addr,  uint8_t *data);
-int cambus_writeb(uint8_t slv_addr, uint8_t reg_addr, uint8_t data);
-int cambus_readw(uint8_t slv_addr, uint8_t reg_addr,  uint16_t *data);
-int cambus_writew(uint8_t slv_addr, uint8_t reg_addr, uint16_t data);
+int cambus_readb(uint8_t slv_addr, uint8_t reg_addr,  uint8_t *reg_data);
+int cambus_writeb(uint8_t slv_addr, uint8_t reg_addr, uint8_t reg_data);
+int cambus_readw(uint8_t slv_addr, uint8_t reg_addr,  uint16_t *reg_data);
+int cambus_writew(uint8_t slv_addr, uint8_t reg_addr, uint16_t reg_data);
 #endif // __CAMBUS_H__
