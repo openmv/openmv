fecf856169c45152fda950a1d97db7dbed3777bf
iabdalkader <i.abdalkader@gmail.com>
2017-10-21 14:17:53 +0200

    Revert "Add generic cam bus read/write."
    This reverts commit d2ebe6f7e28b88296f8b07c7a30706cd94042f39.

--- src/omv/cambus.h
+++ src/omv/cambus.h
@@ -13,6 +13,6 @@ int cambus_init();
 int cambus_scan();
 int cambus_readb(uint8_t slv_addr, uint8_t reg_addr,  uint8_t *reg_data);
 int cambus_writeb(uint8_t slv_addr, uint8_t reg_addr, uint8_t reg_data);
-int cambus_read(uint8_t slv_addr, uint8_t reg_addr,  uint8_t *reg_data, uint8_t reg_size);
-int cambus_write(uint8_t slv_addr, uint8_t reg_addr, uint8_t *reg_data, uint8_t reg_size);
+int cambus_readw(uint8_t slv_addr, uint8_t reg_addr,  uint16_t *reg_data);
+int cambus_writew(uint8_t slv_addr, uint8_t reg_addr, uint16_t reg_data);
 #endif // __CAMBUS_H__
