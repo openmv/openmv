8b3575270618770ed43f95575639aacc09717114
iabdalkader <i.abdalkader@gmail.com>
2017-11-01 22:01:42 +0200

    Fix find_line_segments unit test

--- usr/unittest/script/12-find_line_segments.py
+++ usr/unittest/script/12-find_line_segments.py
@@ -3,14 +3,12 @@ def unittest(data_path, temp_path):
     img = image.Image("unittest/data/shapes.ppm", copy_to_fb=True)
     lines = img.find_line_segments(threshold = 10000, theta_margin = 15, rho_margin = 15, segment_threshold = 100)
     return len(lines) == 6 and\
-    lines[0][0:] ==(22, 40, 22, 73, 33, 17340, 0, 22)    and\
-    lines[1][0:] ==(111, 37, 124, 37, 13, 17340, 90, 39) and\
-    lines[2][0:] ==(24, 39, 56, 39, 32, 17340, 90, 39)   and\
-    lines[3][0:] ==(57, 40, 57, 73, 33, 17340, 0, 57)    and\
-    lines[4][0:] ==(24, 75, 56, 75, 32, 21420, 90, 75)   and\
-    lines[5][0:] ==(112, 75, 126, 75, 14, 21420, 90, 75)
-
-
+    lines[0][0:] == (56, 38, 24, 38, 32, 19, 90, 38)    and\
+    lines[1][0:] == (26, 74, 54, 74, 28, 14, 90, 74)    and\
+    lines[2][0:] == (104, 70, 114, 76, 12, 2, 121, 6)   and\
+    lines[3][0:] == (139, 51, 133, 41, 12, 2, 149, -93) and\
+    lines[4][0:] == (109, 37, 100, 46, 13, 12, 45, 103) and\
+    lines[5][0:] == (127, 75, 138, 64, 16, 1, 45, 143)
 
 
 
