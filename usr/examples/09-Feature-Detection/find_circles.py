ac2d2fc1cd8eef31623d93df06fae35bd959c76a
Kwabena W. Agyeman <kwagyeman@live.com>
2018-05-13 19:31:32 -0400

    Update find circles with min and max radius to run faster.
    This was a user request.

--- usr/examples/09-Feature-Detection/find_circles.py
+++ usr/examples/09-Feature-Detection/find_circles.py
@@ -28,7 +28,11 @@ while(True):
     # `x_margin`, `y_margin`, and `r_margin` control the merging of similar
     # circles in the x, y, and r (radius) directions.
 
-    for c in img.find_circles(threshold = 2000, x_margin = 10, y_margin = 10, r_margin = 10):
+    # r_min, r_max, and r_step control what radiuses of circles are tested.
+    # Shrinking the number of tested circle radiuses yields a big performance boost.
+
+    for c in img.find_circles(threshold = 2000, x_margin = 10, y_margin = 10, r_margin = 10,
+            r_min = 2, r_max = 100, r_step = 2):
         img.draw_circle(c.x(), c.y(), c.r(), color = (255, 0, 0))
         print(c)
 
