19473ef28ca32c5a36bef24fc044114e3ab328a8
Kwabena W. Agyeman <kwagyeman@live.com>
2018-06-07 00:23:13 -0400

    Module Cleanup
    * Updated the header.
    * Removed redundant includes.
    * Switched to ROM pointers.
    * Fixed some spacing issue.

--- src/omv/py/py_nn.c
+++ src/omv/py/py_nn.c
@@ -1,22 +1,15 @@
-/*
- * This file is part of the OpenMV project.
- * Copyright (c) 2013/2014 Ibrahim Abdelkader <i.abdalkader@gmail.com>
+/* This file is part of the OpenMV project.
+ * Copyright (c) 2013-2018 Ibrahim Abdelkader <iabdalkader@openmv.io> & Kwabena W. Agyeman <kwagyeman@openmv.io>
  * This work is licensed under the MIT license, see the file LICENSE for details.
- *
- * NN module.
- *
  */
-#include "mp.h"
+#include <mp.h>
 #include "nn.h"
-#include "imlib.h"
-#include "xalloc.h"
-#include "py_image.h"
 #include "py_helper.h"
-#include "py_assert.h"
+#include "py_image.h"
 #include "omv_boardconfig.h"
-#include <stdbool.h>
 
 #ifdef IMLIB_ENABLE_CNN
+
 static const mp_obj_type_t py_net_type;
 
 typedef struct _py_net_obj_t {
@@ -24,10 +17,10 @@ typedef struct _py_net_obj_t {
     nn_t _cobj;
 } py_net_obj_t;
 
-void *py_net_cobj(mp_obj_t net)
+void *py_net_cobj(mp_obj_t net_obj)
 {
-    PY_ASSERT_TYPE(net, &py_net_type);
-    return &((py_net_obj_t *)net)->_cobj;
+    PY_ASSERT_TYPE(net_obj, &py_net_type);
+    return &((py_net_obj_t *)net_obj)->_cobj;
 }
 
 STATIC mp_obj_t py_net_forward(uint n_args, const mp_obj_t *args, mp_map_t *kw_args)
@@ -50,6 +43,7 @@ STATIC mp_obj_t py_net_forward(uint n_args, const mp_obj_t *args, mp_map_t *kw_a
     }
     return output_list;
 }
+STATIC MP_DEFINE_CONST_FUN_OBJ_KW(py_net_forward_obj, 2, py_net_forward);
 
 STATIC void py_net_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind)
 {
@@ -57,12 +51,8 @@ STATIC void py_net_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kin
     nn_dump_network(py_net_cobj(self));
 }
 
-STATIC MP_DEFINE_CONST_FUN_OBJ_KW(py_net_forward_obj, 2, py_net_forward);
-
-
-static const mp_map_elem_t locals_dict_table[] = {
-    { MP_OBJ_NEW_QSTR(MP_QSTR_forward),  (mp_obj_t)&py_net_forward_obj},
-    { NULL, NULL },
+STATIC const mp_rom_map_elem_t locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_forward), MP_ROM_PTR(&py_net_forward_obj) }
 };
 
 STATIC MP_DEFINE_CONST_DICT(locals_dict, locals_dict_table);
@@ -71,7 +61,7 @@ static const mp_obj_type_t py_net_type = {
     { &mp_type_type },
     .name  = MP_QSTR_Net,
     .print = py_net_print,
-    .locals_dict = (mp_obj_t)&locals_dict,
+    .locals_dict = (mp_obj_t) &locals_dict
 };
 
 static mp_obj_t py_nn_load(mp_obj_t path_obj)
@@ -85,14 +75,15 @@ static mp_obj_t py_nn_load(mp_obj_t path_obj)
 }
 
 STATIC MP_DEFINE_CONST_FUN_OBJ_1(py_nn_load_obj, py_nn_load);
+
 #endif // IMLIB_ENABLE_CNN
 
-static const mp_map_elem_t globals_dict_table[] = {
-    { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_nn) },
+STATIC const mp_rom_map_elem_t globals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_OBJ_NEW_QSTR(MP_QSTR_nn) },
 #ifdef IMLIB_ENABLE_CNN
-    { MP_OBJ_NEW_QSTR(MP_QSTR_load),   (mp_obj_t)&py_nn_load_obj },
+    { MP_ROM_QSTR(MP_QSTR_load),     MP_ROM_PTR(&py_nn_load_obj) },
 #else
-    { MP_OBJ_NEW_QSTR(MP_QSTR_load),   (mp_obj_t)&py_func_unavailable_obj },
+    { MP_ROM_QSTR(MP_QSTR_load),     MP_ROM_PTR(&py_func_unavailable_obj) }
 #endif // IMLIB_ENABLE_CNN
 };
 
@@ -100,5 +91,5 @@ STATIC MP_DEFINE_CONST_DICT(globals_dict, globals_dict_table);
 
 const mp_obj_module_t nn_module = {
     .base = { &mp_type_module },
-    .globals = (mp_obj_t)&globals_dict,
+    .globals = (mp_obj_t) &globals_dict
 };
