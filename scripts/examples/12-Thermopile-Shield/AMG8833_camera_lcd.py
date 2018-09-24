7d88c386c17306deb1a181776ce4537c9b7aa80f
Kwabena W. Agyeman <kwagyeman@live.com>
2018-09-23 23:13:23 -0400

    Heavily re-did all FIR scripts.

---
+++ scripts/examples/12-Thermopile-Shield/AMG8833_camera_lcd.py
@@ -0,0 +1,26 @@
+# AMG8833 Camera Demo
+#
+# This example shows off how to overlay a heatmap onto your OpenMV Cam's
+# live video output from the main camera.
+
+import image, time, fir, gc, lcd
+
+# Initialize the thermal sensor
+fir.init(type=fir.FIR_AMG8833)
+
+# Init the lcd.
+lcd.init()
+
+# FPS clock
+clock = time.clock()
+
+while (True):
+    clock.tick()
+
+    img = fir.snapshot(copy_to_fb=True)
+    gc.collect()
+
+    lcd.display(img)
+
+    # Print FPS.
+    print(clock.fps())
