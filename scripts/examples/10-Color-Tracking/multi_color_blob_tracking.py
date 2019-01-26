c35487aad7864e28929028ed44af116899580f35
Kwabena W. Agyeman <kwagyeman@live.com>
2019-01-26 14:39:02 -0500

    Update example scripts to show off the new features.

--- scripts/examples/10-Color-Tracking/multi_color_blob_tracking.py
+++ scripts/examples/10-Color-Tracking/multi_color_blob_tracking.py
@@ -2,7 +2,7 @@
 #
 # This example shows off multi color blob tracking using the OpenMV Cam.
 
-import sensor, image, time
+import sensor, image, time, math
 
 # Color Tracking Thresholds (L Min, L Max, A Min, A Max, B Min, B Max)
 # The below thresholds track in general red/green things. You may wish to tune them...
@@ -28,6 +28,14 @@ while(True):
     clock.tick()
     img = sensor.snapshot()
     for blob in img.find_blobs(thresholds, pixels_threshold=200, area_threshold=200):
+        # These values depend on the blob not being circular - otherwise they will be shaky.
+        if blob.elongation() > 0.5:
+            img.draw_edges(blob.min_corners(), color=(255,0,0))
+            img.draw_line(blob.major_axis_line(), color=(0,255,0))
+            img.draw_line(blob.minor_axis_line(), color=(0,0,255))
+        # These values are stable all the time.
         img.draw_rectangle(blob.rect())
         img.draw_cross(blob.cx(), blob.cy())
+        # Note - the blob rotation is unique to 0-180 only.
+        img.draw_keypoints([(blob.cx(), blob.cy(), int(math.degrees(blob.rotation())))], size=20)
     print(clock.fps())
