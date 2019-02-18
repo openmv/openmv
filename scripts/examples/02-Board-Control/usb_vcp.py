421b5ddb4183f9c27814dd14c51091199fb7e8a1
iabdalkader <i.abdalkader@gmail.com>
2019-02-18 23:34:09 +0200

    Update USB VCP example.

--- scripts/examples/02-Board-Control/usb_vcp.py
+++ scripts/examples/02-Board-Control/usb_vcp.py
@@ -10,6 +10,7 @@
 # port = '/dev/ttyACM0'
 # sp = serial.Serial(port, baudrate=115200, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
 #             xonxoff=False, rtscts=False, stopbits=serial.STOPBITS_ONE, timeout=None, dsrdtr=True)
+# sp.setDTR(True) # dsrdtr is ignored on Windows.
 # sp.write("snap")
 # sp.flush()
 # size = struct.unpack('<L', sp.read(4))[0]
