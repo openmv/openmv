abde48d77350e0e31a78fade3c1e1a224a1966f7
Kwabena W. Agyeman <kwagyeman@live.com>
2019-03-02 22:04:17 -0500

    Add examples showing off how to get the mean of an object temp.

---
+++ scripts/examples/27-Lepton/lepton_get_object_temp.py
@@ -0,0 +1,57 @@
+# Lepton Get Object Temp Example
+#
+# This example shows off how to get an object's temperature using color tracking.
+
+# By turning the AGC off and setting a max and min temperature range you can make the lepton into
+# a great sensor for seeing objects of a particular temperature. That said, the FLIR lepton is a
+# microblobometer and not a thermoile. So, it needs to re-calibrate itself often (which is called
+# flat-field-correction - FFC). Additionally, microblobmeter devices require pprocessing support
+# onboard to deal with the effects of temperature drift which is called radiometry support.
+
+# FLIR Lepton Shutter Note: FLIR Leptons with radiometry and a shutter will pause the video often
+# as they heatup to re-calibrate. This will happen less and less often as the sensor temperature
+# stablizes. You can force the re-calibration to not happen if you need to via the lepton API.
+# However, it is not recommended because the image will degrade overtime.
+
+# If you are using a LEPTON other than the Lepton 3.5 this script may not work perfectly as other
+# leptons don't have radiometry support or they don't activate their calibration process often
+# enough to deal with temperature changes (FLIR 2.5).
+
+import sensor, image, time, math
+
+# Color Tracking Thresholds (Grayscale Min, Grayscale Max)
+thresholds = [(200, 255)]
+
+# Set the target temp range here
+min_temp_in_celsius = 20.0
+max_temp_in_celsius = 35.0
+
+print("Resetting Lepton...")
+# These settings are applied on reset
+sensor.lepton_set_agc(False)
+sensor.lepton_set_range(min_temp_in_celsius, max_temp_in_celsius)
+sensor.reset()
+print("Lepton Res (%dx%d)" % (sensor.lepton_width(), sensor.lepton_height()))
+print("Radiometry Available: " + ("Yes" if sensor.lepton_type() else "No"))
+
+sensor.set_pixformat(sensor.GRAYSCALE)
+sensor.set_framesize(sensor.QQVGA)
+sensor.skip_frames(time=5000)
+clock = time.clock()
+
+# Only blobs that with more pixels than "pixel_threshold" and more area than "area_threshold" are
+# returned by "find_blobs" below. Change "pixels_threshold" and "area_threshold" if you change the
+# camera resolution. "merge=True" merges all overlapping blobs in the image.
+
+def map_g_to_temp(g):
+    return ((g * (max_temp_in_celsius - min_temp_in_celsius)) / 255.0) + min_temp_in_celsius
+
+while(True):
+    clock.tick()
+    img = sensor.snapshot()
+    for blob in img.find_blobs(thresholds, pixels_threshold=200, area_threshold=200, merge=True):
+        img.draw_rectangle(blob.rect())
+        img.draw_cross(blob.cx(), blob.cy())
+        stats = img.get_stats(roi=blob.rect())
+        img.draw_string(blob.x(), blob.y() - 10, "%.2f C" % map_g_to_temp(stats.mean()), mono_space=False)
+    print("FPS %f - Lepton Temp: %f C" % (clock.fps(), sensor.lepton_temp()))
