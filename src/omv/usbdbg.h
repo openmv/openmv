bf5db7cd3ff5a7cf50160c248b6dc22d590b129a
iabdalkader <i.abdalkader@gmail.com>
2019-03-30 15:44:38 +0200

    Bump firmware version.

--- src/omv/usbdbg.h
+++ src/omv/usbdbg.h
@@ -18,8 +18,8 @@
   * the IDE will Not connect if the major version number is different.
   */
 #define FIRMWARE_VERSION_MAJOR      (3)
-#define FIRMWARE_VERSION_MINOR      (3)
-#define FIRMWARE_VERSION_PATCH      (1)
+#define FIRMWARE_VERSION_MINOR      (4)
+#define FIRMWARE_VERSION_PATCH      (0)
 
 /**
   * To add a new debugging command, increment the last command value used.
