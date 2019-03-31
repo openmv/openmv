4d580033f2e2b48105457124bd9069f8a289adca
iabdalkader <i.abdalkader@gmail.com>
2019-04-01 00:27:12 +0200

    Fix NN unittest.

--- scripts/unittest/script/21-nn_cifar10.py
+++ scripts/unittest/script/21-nn_cifar10.py
@@ -5,11 +5,11 @@ def unittest(data_path, temp_path):
     files = os.listdir(test_dir)
     total = len(files)
     labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-    net = nn.load('/cifar10_fast.network')
+    net = nn.load(data_path+'/cifar10_fast.network')
     for f in sorted(files):
         img = image.Image(test_dir+"/"+f, copy_to_fb=True)
         out = net.forward(img)
         label = labels[out.index(max(out))]
         if (f.split('-')[0] == label):
             match += 1
-    return (total == 21 and match == 19)
+    return ((match / total * 100 ) > 90)
