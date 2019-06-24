e33342354bba0762efedd8177cd866c297b11882
kidswong999 <kidswong999@163.com>
2019-06-24 14:24:21 +0800

    add ov5640 driver

---
+++ src/omv/ov5640.h
@@ -0,0 +1,13 @@
+/*
+ * This file is part of the OpenMV project.
+ * Copyright (c) 2013/2014 Ibrahim Abdelkader <i.abdalkader@gmail.com>
+ * This work is licensed under the MIT license, see the file LICENSE for details.
+ *
+ * OV5640 driver.
+ *
+ */
+#ifndef __OV5640_H__
+#define __OV5640_H__
+#include "sensor.h"
+int ov5640_init(sensor_t *sensor);
+#endif // __OV5640_H__
