eaba3839658cf1975310bb2a78444c60a4cc3422
iabdalkader <i.abdalkader@gmail.com>
2019-10-25 21:57:16 +0200

    WINC1500: Update HTTP/HTTPS client examples.

--- scripts/examples/14-WiFi-Shield/http_client.py
+++ scripts/examples/14-WiFi-Shield/http_client.py
@@ -1,12 +1,13 @@
-# TCP Client Example
-#
-# This example shows how to send and receive TCP traffic with the WiFi shield.
+# Simple HTTP client example.
 
 import network, usocket
 
 # AP info
 SSID='' # Network SSID
 KEY=''  # Network key
+
+PORT = 80
+HOST = "www.google.com"
 
 # Init wlan module and connect to network
 print("Trying to connect... (may take a while)...")
@@ -18,17 +19,18 @@ wlan.connect(SSID, key=KEY, security=wlan.WPA_PSK)
 print(wlan.ifconfig())
 
 # Get addr info via DNS
-addr = usocket.getaddrinfo("www.google.com", 80)[0][4]
+addr = usocket.getaddrinfo(HOST, PORT)[0][4]
+print(addr)
 
 # Create a new socket and connect to addr
 client = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
 client.connect(addr)
 
-# Set timeout to 1s
-client.settimeout(1.0)
+# Set timeout
+client.settimeout(3.0)
 
 # Send HTTP request and recv response
-client.send("GET / HTTP/1.0\r\n\r\n")
+client.send("GET / HTTP/1.1\r\nHost: %s\r\n\r\n"%(HOST))
 print(client.recv(1024))
 
 # Close socket
