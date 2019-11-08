288127c575c7672e3a41d7ada360d56a4853f279
iabdalkader <i.abdalkader@gmail.com>
2019-11-08 03:10:39 +0200

    Update WiFi firmware update script.

--- scripts/examples/14-WiFi-Shield/fw_update.py
+++ scripts/examples/14-WiFi-Shield/fw_update.py
@@ -2,16 +2,13 @@
 #
 # This script updates the ATWINC1500 WiFi module firmware.
 # Copy the firmware image to uSD card before running this script.
-# NOTE: Firmware version 19.5.2 does NOT support ATWINC1500-MR210PA.
+# NOTE: Older fimware versions are no longer supported by the host driver.
+# NOTE: The latest firmware (19.6.1) only works on ATWINC1500-MR210PB.
 
 import network
 
 # Init wlan module in Download mode.
 wlan = network.WINC(mode=network.WINC.MODE_FIRMWARE)
 
-# Start the firmware update process.
-# For ATWINC1500-MR210PA/B
-#wlan.fw_update("/winc_19_4_4.bin")
-
 # For ATWINC1500-MR210PB only.
-wlan.fw_update("/winc_19_5_2.bin")
+wlan.fw_update("/winc_19_6_1.bin")
