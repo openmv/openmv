be700a082ba0e23234256a0876a71fee1f010315
iabdalkader <i.abdalkader@gmail.com>
2019-12-01 20:24:02 +0200

    Update script comments.

--- scripts/examples/14-WiFi-Shield/fw_update.py
+++ scripts/examples/14-WiFi-Shield/fw_update.py
@@ -1,7 +1,11 @@
 # WINC Firmware Update Script.
 #
 # This script updates the ATWINC1500 WiFi module firmware.
-# Copy the firmware image to uSD card before running this script.
+# 1) Copy the firmware image to a FAT32/exFAT SD card.
+# 2) Safe remove/eject the SD card (or umount on Linux).
+# 3) Reset the camera from the IDE.
+# 4) Run this script to update the firmware.
+#
 # NOTE: Older fimware versions are no longer supported by the host driver.
 # NOTE: The latest firmware (19.6.1) only works on ATWINC1500-MR210PB.
 
