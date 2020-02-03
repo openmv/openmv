a601cda422f2903e33bac45609fbe8840c2b915b
iabdalkader <i.abdalkader@gmail.com>
2020-02-03 19:33:02 +0200

    Update unit-tests.
    * Update unit-tests to match the values after disabling YUV table.

--- scripts/unittest/script/02-rgb_to_grayscale.py
+++ scripts/unittest/script/02-rgb_to_grayscale.py
@@ -1,4 +1,4 @@
 def unittest(data_path, temp_path):
     import image
     gs = image.rgb_to_grayscale((120, 200, 120))
-    return  (abs(167-gs) < 2)
+    return  (gs == 166)
--- scripts/unittest/script/11-find_lines.py
+++ scripts/unittest/script/11-find_lines.py
@@ -3,11 +3,7 @@ def unittest(data_path, temp_path):
     img = image.Image("unittest/data/shapes.ppm", copy_to_fb=True)
     lines = img.find_lines(threshold = 10000, theta_margin = 25, rho_margin = 25)
     return len(lines) == 4 and\
-    lines[0][0:-3] == (22, 0, 22, 119, 119)  and\
-    lines[1][0:-3] == (0, 39, 159, 39, 159) and\
-    lines[2][0:-3] == (57, 0, 57, 119, 119)  and\
-    lines[3][0:-3] == (0, 75, 159, 75, 159)  and\
-    lines[0][-2:] == (0, 22)  and\
-    lines[1][-2:] == (90, 39) and\
-    lines[2][-2:] == (0, 57)  and\
-    lines[3][-2:] == (90, 75)
+    lines[0][0:] == (22, 0, 22, 119, 119, 17272, 0, 22)  and\
+    lines[1][0:] == (0, 39, 159, 39, 159, 17272, 90, 39) and\
+    lines[2][0:] == (57, 0, 57, 119, 119, 17272, 0, 57)  and\
+    lines[3][0:] == (0, 75, 159, 75, 159, 21336, 90, 75)
