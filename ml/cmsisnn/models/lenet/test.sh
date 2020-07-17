588f79a62bf70f8070dc2814adb2f6f87eee147d
iabdalkader <i.abdalkader@gmail.com>
2020-07-17 19:56:46 +0200

    Remove legacy CMSIS-NN code and examples.

--- ml/cmsisnn/models/lenet/test.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-TOOLS=./caffe/build/tools
-
-$TOOLS/caffe test \
-    --model=models/lenet/lenet_train_test.prototxt \
-    --weights=models/lenet/lenet_iter_10000.caffemodel $@
