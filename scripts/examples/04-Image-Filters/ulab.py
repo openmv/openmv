ec38ff9ec243b97589d02b684d786743ff49b3cb
iabdalkader <i.abdalkader@gmail.com>
2020-08-24 19:25:12 +0200

    Update ulab example.

--- scripts/examples/04-Image-Filters/ulab.py
+++ scripts/examples/04-Image-Filters/ulab.py
@@ -5,13 +5,14 @@
 # there's not much that can be done about it, lowering the resolution might help.
 
 import sensor, image, time, ulab as np
+from ulab import numerical
 
-sensor.reset()                      # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
-sensor.set_framesize(sensor.QQVGA)   # Set frame size to QVGA (320x240)
-clock = time.clock()                # Create a clock object to track the FPS.
+sensor.reset()                          # Reset and initialize the sensor.
+sensor.set_pixformat(sensor.GRAYSCALE)  # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_framesize(sensor.QQVGA)      # Set frame size to QVGA (320x240)
+clock = time.clock()                    # Create a clock object to track the FPS.
 
 while (True):
     img = sensor.snapshot()         # Take a picture and return the image.
     a = np.array(img, dtype=np.uint8)
-    print("mean: %d std:%d"%(np.mean(a), np.std(a)))
+    print("mean: %d std:%d"%(numerical.mean(a), numerical.std(a)))
