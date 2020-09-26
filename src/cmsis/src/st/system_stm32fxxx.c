d5c214750fa4142ef0afa53bb168dda695c56ef3
iabdalkader <i.abdalkader@gmail.com>
2020-09-26 23:04:25 +0200

    Fix typo.

--- src/cmsis/src/st/system_stm32fxxx.c
+++ src/cmsis/src/st/system_stm32fxxx.c
@@ -168,7 +168,7 @@ void SystemInit(void)
     /* dpgeorge: enable 8-byte stack alignment for IRQ handlers, in accord with EABI */
     SCB->CCR |= SCB_CCR_STKALIGN_Msk;
 
-    #if defined(MCU_SERIES_4) ||  defined(MCU_SERIES_F7)
+    #if defined(MCU_SERIES_F4) ||  defined(MCU_SERIES_F7)
     DBGMCU->CR |= DBGMCU_CR_DBG_SLEEP;
     #elif defined(MCU_SERIES_H7)
     DBGMCU->CR |= DBGMCU_CR_DBG_SLEEPD1;
