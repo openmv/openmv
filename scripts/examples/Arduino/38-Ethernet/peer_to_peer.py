9d9b2bed85560848dbe4d970c63bd1189f9170a9
iabdalkader <i.abdalkader@gmail.com>
2020-11-27 19:51:09 +0200

    PORTENTA: Enable Ethernet.

---
+++ scripts/examples/Arduino/38-Ethernet/peer_to_peer.py
@@ -0,0 +1,15 @@
+# Ethernet LAN Peer to Peer example.
+# On the PC try the following:
+#
+# $> sudo ifconfig eth0 192.168.1.102 up
+# $> ping 192.168.1.102
+
+import network, time
+
+lan = network.LAN()
+lan.active(True)
+lan.ifconfig(('192.168.1.100', '255.255.255.0', '192.168.1.1', '192.168.1.1'))
+
+while (True):
+    # Nothing else to do.
+    time.sleep(1.0)
