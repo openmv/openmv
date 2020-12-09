18323c3d04329396a71583d87690854ffefec071
Jeremy Ellis <keyfreemusic@gmail.com>
2020-12-08 22:00:07 -0800

    Update helloworld.py

--- scripts/examples/Arduino/01-Basics/helloworld.py
+++ scripts/examples/Arduino/01-Basics/helloworld.py
@@ -5,7 +5,7 @@
 import sensor, image, time
 
 sensor.reset()                      # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.RGB565) # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QVGA)   # Set frame size to QVGA (320x240)
 sensor.skip_frames(time = 2000)     # Wait for settings take effect.
 clock = time.clock()                # Create a clock object to track the FPS.
