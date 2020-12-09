d2b06a9a8765431ef286292e5dca40cf44b2a441
Jeremy Ellis <keyfreemusic@gmail.com>
2020-12-08 22:22:43 -0800

    Update selective_search.py

--- scripts/examples/Arduino/09-Feature-Detection/selective_search.py
+++ scripts/examples/Arduino/09-Feature-Detection/selective_search.py
@@ -4,7 +4,7 @@ import sensor, image, time
 from random import randint
 
 sensor.reset()                      # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.RGB565) # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QVGA)   # Set frame size to QVGA (320x240)
 sensor.skip_frames(time = 2000)     # Wait for settings take effect.
 sensor.set_auto_gain(False)
