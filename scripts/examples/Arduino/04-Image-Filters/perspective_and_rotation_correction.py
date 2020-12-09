66089aeb9477abcfea7eec055ffbef9bff746763
Jeremy Ellis <keyfreemusic@gmail.com>
2020-12-08 22:15:05 -0800

    Update perspective_and_rotation_correction.py

--- scripts/examples/Arduino/04-Image-Filters/perspective_and_rotation_correction.py
+++ scripts/examples/Arduino/04-Image-Filters/perspective_and_rotation_correction.py
@@ -7,7 +7,7 @@
 import sensor, image, time
 
 sensor.reset()
-sensor.set_pixformat(sensor.RGB565)
+sensor.set_pixformat(sensor.GRAYSCALE)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
 clock = time.clock()
