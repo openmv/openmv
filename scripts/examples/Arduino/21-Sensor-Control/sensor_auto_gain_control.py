efe96cdd1723c12ea07bc040e04f9335ca838eb9
Jeremy Ellis <keyfreemusic@gmail.com>
2020-12-08 22:31:47 -0800

    Update sensor_auto_gain_control.py

--- scripts/examples/Arduino/21-Sensor-Control/sensor_auto_gain_control.py
+++ scripts/examples/Arduino/21-Sensor-Control/sensor_auto_gain_control.py
@@ -25,7 +25,7 @@
 import sensor, image, time
 
 sensor.reset()                      # Reset and initialize the sensor.
-sensor.set_pixformat(sensor.RGB565) # Set pixel format to RGB565 (or GRAYSCALE)
+sensor.set_pixformat(sensor.GRAYSCALE) # Set pixel format to RGB565 (or GRAYSCALE)
 sensor.set_framesize(sensor.QVGA)   # Set frame size to QVGA (320x240)
 
 # The gain db ceiling maxes out at about 24 db for the OV7725 sensor.
