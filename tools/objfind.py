76b6eafd9b5b9fbf23462f549b4f4c1d9d24196f
iabdalkader <i.abdalkader@gmail.com>
2021-01-20 23:43:11 +0200

    Update util script.

--- tools/objfind.py
+++ tools/objfind.py
@@ -17,9 +17,14 @@ if __name__ == "__main__":
         print('objfind.py <sort key (text, bss or data)>')
         sys.exit(1)
     
+    if len(sys.argv) == 3:
+        cwd = sys.argv[2]
+    else:
+        cwd = '.'
+
     objfiles = []
     sortkey = sys.argv[1]
-    for root, dirs, files in os.walk("."):
+    for root, dirs, files in os.walk(cwd):
         for file in files:
             if file.endswith(".o"):
                  objfiles.append({'path':os.path.join(root, file)})
