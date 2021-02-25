288d0f72eb6b8a11c00aa8c731af50a01fcf5646
iabdalkader <i.abdalkader@gmail.com>
2021-02-25 23:48:04 +0200

    NANO: Reduce ISC I2C frequency to standard.

--- src/omv/boards/NANO33/omv_boardconfig.h
+++ src/omv/boards/NANO33/omv_boardconfig.h
@@ -104,7 +104,7 @@
 #define ISC_I2C_ID              (0)
 #define ISC_I2C_SCL_PIN         (2)
 #define ISC_I2C_SDA_PIN         (31)
-#define ISC_I2C_SPEED           (CAMBUS_SPEED_FULL)
+#define ISC_I2C_SPEED           (CAMBUS_SPEED_STANDARD)
 
 // I2C0
 #define TWI0_ID                 (0)
