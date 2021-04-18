7bc223d6b9cbe8f98972606ecc4f35962f23bbb8
iabdalkader <i.abdalkader@gmail.com>
2021-04-18 23:34:20 +0200

    Update unit-tests.

--- scripts/unittest/script/09-find_blobs.py
+++ scripts/unittest/script/09-find_blobs.py
@@ -7,6 +7,6 @@ def unittest(data_path, temp_path):
     img = image.Image("unittest/data/blobs.ppm", copy_to_fb=True)
 
     blobs = img.find_blobs(thresholds, pixels_threshold=2000, area_threshold=200)
-    return  [int(x) for x in blobs[0][0:-5]] == [122, 41, 97, 82, 6266, 168, 82] and\
-            [int(x) for x in blobs[1][0:-5]] == [44, 42, 78, 88, 5162, 80, 84]   and\
-            [int(x) for x in blobs[2][0:-5]] == [210, 40, 72, 83, 3930, 249, 76]
+    return  [int(x) for x in blobs[0][0:-5]] == [122, 41, 98, 82, 6260, 168, 82] and\
+            [int(x) for x in blobs[1][0:-5]] == [44, 42, 78, 88, 5158, 80, 84]   and\
+            [int(x) for x in blobs[2][0:-5]] == [210, 40, 72, 82, 3986, 248, 77]
