8ce2bb6d9bd6fa5983735859b180b70e25cd23a1
iabdalkader <i.abdalkader@gmail.com>
2021-04-24 20:56:50 +0200

    PORTENTA: Reduce the heap to fit Eth descriptors.
    * Must reduce the heap for eth descriptors to fit.

--- src/omv/boards/PORTENTA/omv_boardconfig.h
+++ src/omv/boards/PORTENTA/omv_boardconfig.h
@@ -152,7 +152,7 @@
 #define OMV_FB_SIZE             (4M)       // FB memory: header + VGA/GS image
 #define OMV_FB_ALLOC_SIZE       (3M)       // minimum fb alloc size
 #define OMV_STACK_SIZE          (64K)
-#define OMV_HEAP_SIZE           (180K)
+#define OMV_HEAP_SIZE           (170K)
 #define OMV_SDRAM_SIZE          (8 * 1024 * 1024) // This needs to be here for UVC firmware.
 
 #define OMV_LINE_BUF_SIZE       (11 * 1024) // Image line buffer round(2592 * 2BPP * 2 buffers).
