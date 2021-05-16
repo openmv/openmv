e52f0ecebad868e109a13475516468c15bf4a437
iabdalkader <i.abdalkader@gmail.com>
2021-05-16 23:23:08 +0200

    Update OV767x driver.

--- src/omv/boards/NANO33/omv_boardconfig.h
+++ src/omv/boards/NANO33/omv_boardconfig.h
@@ -29,12 +29,6 @@
 // Sensor external clock timer frequency.
 #define OMV_XCLK_FREQUENCY      (12000000)
 
-// Sensor PLL register value.
-#define OMV_OV7725_PLL_CONFIG   (0x41)  // x4
-
-// Sensor Banding Filter Value
-#define OMV_OV7725_BANDING      (0x7F)
-
 // RAW buffer size
 #define OMV_RAW_BUF_SIZE        (131072)
 
@@ -51,6 +45,9 @@
 #define OMV_ENABLE_MT9V034      (0)
 #define OMV_ENABLE_LEPTON       (0)
 #define OMV_ENABLE_HM01B0       (0)
+
+// Set which OV767x sensor is used
+#define OMV_OV7670_VERSION      (75)
 
 // Enable sensor features
 #define OMV_ENABLE_OV5640_AF    (0)
