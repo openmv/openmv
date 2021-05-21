30d124d7863b50ff9bde78112912b8499a7315e6
iabdalkader <i.abdalkader@gmail.com>
2021-05-21 23:21:19 +0200

    GC2145 updates.

--- src/omv/boards/PORTENTA/omv_boardconfig.h
+++ src/omv/boards/PORTENTA/omv_boardconfig.h
@@ -59,6 +59,7 @@
 #define OMV_ENABLE_MT9V034      (0)
 #define OMV_ENABLE_LEPTON       (0)
 #define OMV_ENABLE_HM01B0       (1)
+#define OMV_ENABLE_GC2145       (1)
 
 // Enable sensor features
 #define OMV_ENABLE_OV5640_AF    (0)
