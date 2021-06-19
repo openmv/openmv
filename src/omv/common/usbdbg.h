012313b791992e23f22eacd5f9265da4b5b45816
iabdalkader <i.abdalkader@gmail.com>
2021-06-19 22:47:57 +0200

    Bump firmware version.

--- src/omv/common/usbdbg.h
+++ src/omv/common/usbdbg.h
@@ -16,8 +16,8 @@
   *
   */
 #define FIRMWARE_VERSION_MAJOR      (4)
-#define FIRMWARE_VERSION_MINOR      (0)
-#define FIRMWARE_VERSION_PATCH      (2)
+#define FIRMWARE_VERSION_MINOR      (1)
+#define FIRMWARE_VERSION_PATCH      (0)
 
 /**
   * To add a new debugging command, increment the last command value used.
