935241fa29bb2a2220926822995bd13d3c5d836c
Ibrahim Abd Elkader <i.abdalkader@gmail.com>
2021-06-29 01:49:12 +0200

    Update src/omv/imlib/jpegd.c

--- src/omv/imlib/jpegd.c
+++ src/omv/imlib/jpegd.c
@@ -3,19 +3,10 @@
  *
  * Copyright (c) 2013-2021 Ibrahim Abdelkader <iabdalkader@openmv.io>
  * Copyright (c) 2013-2021 Kwabena W. Agyeman <kwagyeman@openmv.io>
- * Copyright (c) 2013-2021 Larry Bank <bitbank@pobox.com>
  *
- * Copyright 2020 BitBank Software, Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * This work is licensed under the MIT license, see the file LICENSE for details.
  *
- * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Baseline JPEG decoder.
  */
 #include "ff_wrapper.h"
 #include "imlib.h"
