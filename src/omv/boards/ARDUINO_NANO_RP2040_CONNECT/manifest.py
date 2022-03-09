c0dd6a3b82a3048206a7306968f9ba2e4a7efc30
iabdalkader <i.abdalkader@gmail.com>
2022-03-10 01:30:41 +0200

    boards/NANO-RP2040: Update manifest.

--- src/omv/boards/ARDUINO_NANO_RP2040_CONNECT/manifest.py
+++ src/omv/boards/ARDUINO_NANO_RP2040_CONNECT/manifest.py
@@ -1,5 +1,7 @@
 freeze ("$(PORT_DIR)/modules")
+freeze("$(MPY_DIR)/drivers/onewire")
+freeze("$(MPY_DIR)/drivers/dht", "dht.py")
 include("$(MPY_DIR)/extmod/uasyncio/manifest.py")
+include("$(MPY_DIR)/drivers/neopixel/manifest.py")
 freeze ("$(MPY_LIB_DIR)/", "lsm6dsox.py")
 freeze ("$(MPY_LIB_DIR)/", "ble_advertising.py")
-freeze ("$(MPY_DIR)/drivers/dht", "dht.py")
