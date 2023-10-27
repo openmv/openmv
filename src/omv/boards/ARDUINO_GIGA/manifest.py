7bcdca8241fcd6a6680e21ca63ce22b1632699c3
iabdalkader <i.abdalkader@gmail.com>
2023-10-27 10:46:02 +0200

    scripts/libraries: Refactor display module.

--- src/omv/boards/ARDUINO_GIGA/manifest.py
+++ src/omv/boards/ARDUINO_GIGA/manifest.py
@@ -7,6 +7,7 @@ require("neopixel")
 freeze ("$(OMV_LIB_DIR)/", "modbus.py")
 freeze ("$(OMV_LIB_DIR)/", "pid.py")
 freeze ("$(OMV_LIB_DIR)/", "gt911.py")
+freeze ("$(OMV_LIB_DIR)/", "st7701.py")
 freeze ("$(OMV_LIB_DIR)/", "machine.py")
 freeze ("$(OMV_LIB_DIR)/", "display.py")
 
