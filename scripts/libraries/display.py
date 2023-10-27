ad44d4533f15fbb9eb5d2d0b53357957dc87242d
iabdalkader <i.abdalkader@gmail.com>
2023-10-27 10:43:45 +0200

    scripts/libraries: Add Python DAC and PWM backlight controllers.

--- scripts/libraries/display.py
+++ scripts/libraries/display.py
@@ -5,13 +5,39 @@
 #
 # This work is licensed under the MIT license, see the file LICENSE for details.
 #
-# This is an extension to the display C user-module. Add any display-related
-# drivers here, and freeze this module in the board's manifest, and those
-# drivers will be importable from display.
+# This is an extension to the display C user-module. Add or import any display-related
+# drivers here, and freeze this module in the board's manifest, and those drivers will
+# be importable from display.
 
 from udisplay import *
 import time
 
+class DACBacklight:
+    def __init__(self, channel, bits=8):
+        from pyb import DAC
+        self.bits = bits
+        self.dac = DAC(channel, bits=bits, buffering=True)
+        self.backlight(100)
+
+    def deinit(self):
+        self.dac.deinit()
+
+    def backlight(self, value):
+        self.dac.write(int(value / 100 * (2 ** self.bits - 1)))
+
+class PWMBacklight:
+    def __init__(self, pin, timer=3, channel=3, frequency=200):
+        from machine import Pin
+        from pyb import Timer
+        self.timer = Timer(timer, freq=frequency)
+        self.channel = self.timer.channel(channel, Timer.PWM, pin=Pin(pin))
+        self.backlight(100)
+
+    def deinit(self):
+        self.timer.deinit()
+
+    def backlight(self, value):
+        self.channel.pulse_width_percent(value * 100 // 100)
 
 class ST7701:
     DSI_CMD2_BKX_SEL = const(0xFF)
