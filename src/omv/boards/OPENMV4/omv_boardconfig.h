3b263c3a68a510e02cc98e0cbcd629aa184d9b40
Kwabena W. Agyeman <kwagyeman@live.com>
2024-01-12 19:34:16 -0800

    boards: Enable OV5640 Autofocus firmware and add example.

--- src/omv/boards/OPENMV4/omv_boardconfig.h
+++ src/omv/boards/OPENMV4/omv_boardconfig.h
@@ -77,7 +77,7 @@
 #define OMV_OV7670_CLKRC                      (0)
 
 // Enable sensor features
-#define OMV_ENABLE_OV5640_AF                  (0)
+#define OMV_ENABLE_OV5640_AF                  (1)
 
 // Enable WiFi debug
 #define OMV_ENABLE_WIFIDBG                    (1)
