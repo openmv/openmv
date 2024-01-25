e7ca0955f980395fcedcb5076e10bea903b2a4b3
Kwabena W. Agyeman <kwagyeman@live.com>
2024-01-24 21:18:38 -0800

    scripts/examples: Reorganize OpenMV Board examples.

---
+++ scripts/examples/50-OpenMV-Boards/50-STM32-Boards/80-Low-Power/deep_sleep.py
@@ -0,0 +1,40 @@
+# This work is licensed under the MIT license.
+# Copyright (c) 2013-2023 OpenMV LLC. All rights reserved.
+# https://github.com/openmv/openmv/blob/master/LICENSE
+#
+# Deep Sleep Mode Example
+# This example demonstrates the low-power deep sleep mode plus sensor shutdown.
+# Note the camera will reset after wake-up from deep sleep. To find out if the cause of reset
+# is deep sleep, call the machine.reset_cause() function and test for machine.DEEPSLEEP_RESET
+import machine
+import sensor
+
+# Create and init RTC object.
+rtc = machine.RTC()
+
+# (year, month, day[, hour[, minute[, second[, microsecond[, tzinfo]]]]])
+rtc.datetime((2014, 5, 1, 4, 13, 0, 0, 0))
+
+# Print RTC info.
+print(rtc.datetime())
+
+sensor.reset()
+
+# Enable sensor softsleep
+sensor.sleep(True)
+
+# Optionally bypass the regulator on OV7725
+# for the lowest possible power consumption.
+if sensor.get_id() == sensor.OV7725:
+    # Bypass internal regulator
+    sensor.__write_reg(0x4F, 0x18)
+
+# Shutdown the sensor (pulls PWDN high).
+sensor.shutdown(True)
+
+# Enable RTC interrupts every 30 seconds.
+# Note the camera will RESET after wakeup from Deepsleep Mode.
+rtc.wakeup(30000)
+
+# Enter Deepsleep Mode.
+machine.deepsleep()
