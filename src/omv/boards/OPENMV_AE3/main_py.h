13901ea5b43d95ba9e930a77ff37c2dc69dbbb18
iabdalkader <i.abdalkader@gmail.com>
2024-02-06 21:48:16 +0200

    boards: Add OPENMV_AE3 board config files.
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

---
+++ src/omv/boards/OPENMV_AE3/main_py.h
@@ -0,0 +1,14 @@
+static const char fresh_main_py[] =
+    "# main.py -- put your code here!\n"
+    "import machine, time\n"
+    "led = machine.LED(\"LED_BLUE\")\n"
+    "while (True):\n"
+    "   led.on()\n"
+    "   time.sleep_ms(150)\n"
+    "   led.off()\n"
+    "   time.sleep_ms(100)\n"
+    "   led.on()\n"
+    "   time.sleep_ms(150)\n"
+    "   led.off()\n"
+    "   time.sleep_ms(600)\n"
+;
