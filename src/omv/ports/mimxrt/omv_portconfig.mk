c79b3aecbb6518f7eeb0d20ee9e5142cbffa1ff8
iabdalkader <i.abdalkader@gmail.com>
2024-06-27 18:21:30 +0200

    ports/mimxrt: Update port Makefiles to support the new TFLM.

--- src/omv/ports/mimxrt/omv_portconfig.mk
+++ src/omv/ports/mimxrt/omv_portconfig.mk
@@ -5,13 +5,26 @@ LDSCRIPT  ?= mimxrt
 
 # Compiler Flags
 # TODO: -Wdouble-promotion
-CFLAGS += -std=gnu99 -Wall -Werror -Warray-bounds -nostartfiles -fdata-sections -ffunction-sections \
-          -fno-inline-small-functions -mfloat-abi=$(FABI) -mthumb -mcpu=$(CPU) -mtune=$(CPU) -mfpu=$(FPU)
+CFLAGS += -std=gnu99 \
+          -Wall \
+          -Werror \
+          -Warray-bounds \
+          -nostartfiles \
+          -fdata-sections \
+          -ffunction-sections \
+          -fno-inline-small-functions \
+          -mfloat-abi=$(FABI) \
+          -mthumb \
+          -mcpu=$(CPU) \
+          -mtune=$(CPU) \
+          -mfpu=$(FPU)
+
 # TODO: FIX HSE
-CFLAGS += -DCPU_$(MCU_VARIANT)\
-          -D$(TARGET)\
-          -DARM_NN_TRUNCATE\
-          -D__FPU_PRESENT=1 -D__VFP_FP__\
+CFLAGS += -DCPU_$(MCU_VARIANT) \
+          -D$(TARGET) \
+          -DARM_NN_TRUNCATE \
+          -D__FPU_PRESENT=1 \
+          -D__VFP_FP__ \
           -DHSE_VALUE=$(OMV_HSE_VALUE) \
           -DMICROPY_PY_MACHINE_SDCARD=1 \
           -DXIP_EXTERNAL_FLASH=1 \
@@ -21,11 +34,23 @@ CFLAGS += -DCPU_$(MCU_VARIANT)\
 	      -DCPU_HEADER_H='<$(MCU_SERIES).h>' \
 	      -DCMSIS_MCU_H=$(CMSIS_MCU_H) \
 	      -DCLOCK_CONFIG_H='<boards/$(MCU_SERIES)_clock_config.h>' \
-          -DCSI_DRIVER_FRAG_MODE=1\
-          -D__START=main\
+          -DCSI_DRIVER_FRAG_MODE=1 \
+          -D__START=main \
           -D__STARTUP_CLEAR_BSS\
           -D__STARTUP_INITIALIZE_RAMFUNCTION\
-          $(OMV_BOARD_EXTRA_CFLAGS)\
+          $(OMV_BOARD_EXTRA_CFLAGS)
+
+# Linker Flags
+LDFLAGS = -mthumb \
+          -mcpu=$(CPU) \
+          -mfpu=$(FPU) \
+          -mfloat-abi=hard \
+          -mabi=aapcs-linux \
+          -Wl,--print-memory-usage \
+          -Wl,--gc-sections \
+          -Wl,-T$(BUILD)/$(LDSCRIPT).lds \
+          -Wl,--wrap=tud_cdc_rx_cb \
+          -Wl,--wrap=mp_hal_stdout_tx_strn
 
 HAL_CFLAGS += -I$(TOP_DIR)/$(CMSIS_DIR)/include/
 HAL_CFLAGS += -I$(TOP_DIR)/$(CMSIS_DIR)/include/mimxrt
@@ -50,8 +75,10 @@ else
 MPY_CFLAGS += -DMICROPY_PY_SSL=1 -DMICROPY_SSL_MBEDTLS=1
 MICROPY_ARGS += MICROPY_PY_SSL=1 MICROPY_SSL_MBEDTLS=1
 endif
-MICROPY_ARGS += MCU_DIR=$(TOP_DIR)/$(HAL_DIR) CMSIS_DIR=$(TOP_DIR)/$(CMSIS_DIR)\
-                SUPPORTS_HARDWARE_FP_SINGLE=1 MICROPY_VFS_LFS2=0
+MICROPY_ARGS += MCU_DIR=$(TOP_DIR)/$(HAL_DIR) \
+                CMSIS_DIR=$(TOP_DIR)/$(CMSIS_DIR)\
+                SUPPORTS_HARDWARE_FP_SINGLE=1 \
+                MICROPY_VFS_LFS2=0
 
 OMV_CFLAGS += -I$(OMV_BOARD_CONFIG_DIR)
 OMV_CFLAGS += -I$(TOP_DIR)/$(OMV_DIR)/
@@ -73,36 +100,18 @@ OMV_CFLAGS += -I$(TOP_DIR)/$(MLX90621_DIR)/include/
 OMV_CFLAGS += -I$(TOP_DIR)/$(MLX90640_DIR)/include/
 OMV_CFLAGS += -I$(TOP_DIR)/$(MLX90641_DIR)/include/
 OMV_CFLAGS += -I$(TOP_DIR)/$(PIXART_DIR)/include/
-OMV_CFLAGS += -I$(TOP_DIR)/$(TENSORFLOW_DIR)/
 OMV_CFLAGS += -I$(BUILD)/$(TENSORFLOW_DIR)/
-OMV_CFLAGS += -I$(TOP_DIR)/$(LIBPDM_DIR)/
-
-ifeq ($(OMV_ENABLE_UVC), 1)
-UVC_CFLAGS := $(CFLAGS) $(HAL_CFLAGS)
-UVC_CFLAGS += -I$(OMV_BOARD_CONFIG_DIR)
-UVC_CFLAGS += -I$(TOP_DIR)/$(UVC_DIR)/include/
-UVC_CFLAGS += $(OMV_CFLAGS) $(MPY_CFLAGS)
-# Linker Flags
-UVC_LDFLAGS = -mcpu=$(CPU) -mabi=aapcs-linux -mthumb -mfpu=$(FPU) -mfloat-abi=hard -nostdlib \
-              -Wl,--print-memory-usage -Wl,--gc-sections -Wl,-T$(BUILD)/$(UVC_DIR)/$(LD_SCRIPT).lds
-endif
 
 CFLAGS += $(HAL_CFLAGS) $(MPY_CFLAGS) $(OMV_CFLAGS)
-# Linker Flags
-LDFLAGS = -mcpu=$(CPU) -mabi=aapcs-linux -mthumb -mfpu=$(FPU) -mfloat-abi=hard -nostdlib \
-          -Wl,--print-memory-usage -Wl,--gc-sections -Wl,-T$(BUILD)/$(LDSCRIPT).lds \
-          -Wl,--wrap=tud_cdc_rx_cb -Wl,--wrap=mp_hal_stdout_tx_strn
 
 #------------- Libraries ----------------#
-LIBS += $(TOP_DIR)/$(TENSORFLOW_DIR)/$(CPU)/libtf*.a
 ifeq ($(MICROPY_PY_AUDIO), 1)
 LIBS += $(TOP_DIR)/$(LIBPDM_DIR)/libPDMFilter_CM7_GCC_wc32.a
 endif
+LIBS += $(TOP_DIR)/$(TENSORFLOW_DIR)/libtflm/lib/libtflm-$(CPU)+fp-release.a
 
 #------------- Firmware Objects ----------------#
-FIRM_OBJ += $(wildcard $(BUILD)/$(CMSIS_DIR)/src/dsp/CommonTables/*.o)
-FIRM_OBJ += $(wildcard $(BUILD)/$(CMSIS_DIR)/src/dsp/FastMathFunctions/*.o)
-
+FIRM_OBJ += $(wildcard $(BUILD)/$(CMSIS_DIR)/src/dsp/*/*.o)
 FIRM_OBJ += $(wildcard $(BUILD)/$(HAL_DIR)/drivers/*.o)
 FIRM_OBJ += $(wildcard $(BUILD)/$(LEPTON_DIR)/src/*.o)
 ifeq ($(MICROPY_PY_IMU), 1)
@@ -114,6 +123,7 @@ FIRM_OBJ += $(wildcard $(BUILD)/$(MLX90640_DIR)/src/*.o)
 FIRM_OBJ += $(wildcard $(BUILD)/$(MLX90641_DIR)/src/*.o)
 FIRM_OBJ += $(wildcard $(BUILD)/$(VL53L5CX_DIR)/src/*.o)
 FIRM_OBJ += $(wildcard $(BUILD)/$(PIXART_DIR)/src/*.o)
+FIRM_OBJ += $(wildcard $(BUILD)/$(TENSORFLOW_DIR)/*.o)
 
 #------------- OpenMV Objects ----------------#
 FIRM_OBJ += $(addprefix $(BUILD)/$(CMSIS_DIR)/src/, \
@@ -218,7 +228,6 @@ FIRM_OBJ += $(addprefix $(BUILD)/$(OMV_DIR)/imlib/, \
 	zbar.o                      \
    )
 
-FIRM_OBJ += $(wildcard $(BUILD)/$(TENSORFLOW_DIR)/*.o)
 FIRM_OBJ += $(wildcard $(BUILD)/$(OMV_DIR)/ports/$(PORT)/*.o)
 
 #------------- MicroPy Objects -------------------#
@@ -294,36 +303,6 @@ FIRM_OBJ += $(addprefix $(BUILD)/$(MICROPY_DIR)/shared/,\
 	runtime/softtimer.o         \
 	timeutils/timeutils.o       \
 	readline/readline.o         \
-	)
-
-FIRM_OBJ += $(addprefix $(BUILD)/$(MICROPY_DIR)/lib/libm/,\
-	math.o              \
-	roundf.o            \
-	asinfacosf.o        \
-	atanf.o             \
-	atan2f.o            \
-	fmodf.o             \
-	log1pf.o            \
-	acoshf.o            \
-	asinhf.o            \
-	atanhf.o            \
-	kf_rem_pio2.o       \
-	kf_sin.o            \
-	kf_cos.o            \
-	kf_tan.o            \
-	ef_rem_pio2.o       \
-	erf_lgamma.o        \
-	sf_sin.o            \
-	sf_cos.o            \
-	sf_tan.o            \
-	sf_frexp.o          \
-	sf_modf.o           \
-	sf_ldexp.o          \
-	sf_erf.o            \
-	wf_lgamma.o         \
-	wf_tgamma.o         \
-	nearbyintf.o        \
-	thumb_vfp_sqrtf.o   \
 	)
 
 FIRM_OBJ += $(wildcard $(BUILD)/$(MICROPY_DIR)/lib/mbedtls/library/*.o)
@@ -481,83 +460,11 @@ FIRM_OBJ += $(addprefix $(BUILD)/$(MICROPY_DIR)/,\
 	)
 endif
 
-ifeq ($(OMV_ENABLE_UVC), 1)
-UVC = uvc
-# UVC object files
-UVC_OBJ += $(wildcard $(BUILD)/$(UVC_DIR)/src/*.o)
-UVC_OBJ += $(wildcard $(BUILD)/$(HAL_DIR)/drivers/*.o)
-UVC_OBJ += $(addprefix $(BUILD)/$(CMSIS_DIR)/src/,\
-	$(STARTUP).o                                \
-	$(SYSTEM).o                                 \
-	)
-
-UVC_OBJ += $(addprefix $(BUILD)/$(OMV_DIR)/alloc/, \
-	fb_alloc.o                              \
-	unaligned_memcpy.o                      \
-	)
-
-UVC_OBJ += $(addprefix $(BUILD)/$(OMV_DIR)/common/, \
-	array.o                                 \
-	trace.o                                 \
-	mutex.o                                 \
-	vospi.o                                 \
-	sensor_utils.o                          \
-	)
-
-UVC_OBJ += $(addprefix $(BUILD)/$(OMV_DIR)/sensors/, \
-	ov2640.o                                \
-	ov5640.o                                \
-	ov7690.o                                \
-	ov7670.o                                \
-	ov7725.o                                \
-	ov9650.o                                \
-	mt9v0xx.o                               \
-	mt9m114.o                               \
-	lepton.o                                \
-	hm01b0.o                                \
-	hm0360.o                                \
-	gc2145.o                                \
-	pag7920.o                               \
-	paj6100.o                               \
-	frogeye2020.o                           \
-	)
-
-UVC_OBJ += $(addprefix $(BUILD)/$(OMV_DIR)/imlib/,\
-	lab_tab.o                               \
-	xyz_tab.o                               \
-	rainbow_tab.o                           \
-	jpeg.o                                  \
-	fmath.o                                 \
-	imlib.o                                 \
-	framebuffer.o                           \
-	)
-
-UVC_OBJ += $(addprefix $(BUILD)/$(OMV_DIR)/ports/$(PORT),\
-	sensor.o                                \
-	soft_i2c.o                              \
-	ulpi.o                                  \
-	omv_spi.o                               \
-	omv_i2c.o                               \
-	omv_gpio.o                              \
-	mimxrt_hal.o                            \
-	)
-
-UVC_OBJ += $(wildcard $(BUILD)/$(LEPTON_DIR)/src/*.o)
-ifeq ($(MICROPY_PY_IMU), 1)
-UVC_OBJ += $(wildcard $(BUILD)/$(LSM6DS3_DIR)/src/*.o)
-UVC_OBJ += $(wildcard $(BUILD)/$(LSM6DSOX_DIR)/src/*.o)
-endif
-UVC_OBJ += $(wildcard $(BUILD)/$(MLX90621_DIR)/src/*.o)
-UVC_OBJ += $(wildcard $(BUILD)/$(MLX90640_DIR)/src/*.o)
-UVC_OBJ += $(wildcard $(BUILD)/$(MLX90641_DIR)/src/*.o)
-UVC_OBJ += $(wildcard $(BUILD)/$(VL53L5CX_DIR)/src/*.o)
-UVC_OBJ += $(wildcard $(BUILD)/$(PIXART_DIR)/src/*.o)
-endif
-
 ###################################################
 #Export Variables
 export Q
 export CC
+export CXX
 export AS
 export LD
 export AR
@@ -591,10 +498,10 @@ $(FW_DIR):
 	$(MKDIR) -p $@
 
 FIRMWARE_OBJS: | $(BUILD) $(FW_DIR)
-	$(MAKE)  -C $(CMSIS_DIR)                 BUILD=$(BUILD)/$(CMSIS_DIR)        CFLAGS="$(CFLAGS) -fno-strict-aliasing -MMD"
-	$(MAKE)  -C $(TENSORFLOW_DIR)            BUILD=$(BUILD)/$(TENSORFLOW_DIR)   CFLAGS="$(CFLAGS) -MMD"
-	$(MAKE)  -C $(MICROPY_DIR)/ports/$(PORT) BUILD=$(BUILD)/$(MICROPY_DIR)      $(MICROPY_ARGS)
+	$(MAKE)  -C $(CMSIS_DIR)                 BUILD=$(BUILD)/$(CMSIS_DIR)        CFLAGS="$(CFLAGS) -MMD"
 	$(MAKE)  -C $(HAL_DIR)                   BUILD=$(BUILD)/$(HAL_DIR)          CFLAGS="$(CFLAGS) -MMD"
+	$(MAKE)  -C $(TENSORFLOW_DIR)            BUILD=$(BUILD)/$(TENSORFLOW_DIR)   CFLAGS="$(CFLAGS) -MMD" headers
+	$(MAKE)  -C $(MICROPY_DIR)/ports/$(PORT) BUILD=$(BUILD)/$(MICROPY_DIR)      $(MICROPY_ARGS)
 	$(MAKE)  -C $(LEPTON_DIR)                BUILD=$(BUILD)/$(LEPTON_DIR)       CFLAGS="$(CFLAGS) -MMD"
 ifeq ($(MICROPY_PY_IMU), 1)
 	$(MAKE)  -C $(LSM6DS3_DIR)               BUILD=$(BUILD)/$(LSM6DS3_DIR)      CFLAGS="$(CFLAGS) -MMD"
@@ -605,30 +512,18 @@ endif
 	$(MAKE)  -C $(MLX90641_DIR)              BUILD=$(BUILD)/$(MLX90641_DIR)     CFLAGS="$(CFLAGS) -MMD"
 	$(MAKE)  -C $(VL53L5CX_DIR)              BUILD=$(BUILD)/$(VL53L5CX_DIR)     CFLAGS="$(CFLAGS) -MMD"
 	$(MAKE)  -C $(PIXART_DIR)                BUILD=$(BUILD)/$(PIXART_DIR)       CFLAGS="$(CFLAGS) -MMD"
+	$(MAKE)  -C $(TENSORFLOW_DIR)            BUILD=$(BUILD)/$(TENSORFLOW_DIR)   CFLAGS="$(CFLAGS) -MMD" all
 	$(MAKE)  -C $(OMV_DIR)                   BUILD=$(BUILD)/$(OMV_DIR)          CFLAGS="$(CFLAGS) -MMD"
-ifeq ($(OMV_ENABLE_UVC), 1)
-UVC_OBJS: FIRMWARE_OBJS
-	$(MAKE)  -C $(UVC_DIR)                   BUILD=$(BUILD)/$(UVC_DIR)          CFLAGS="$(UVC_CFLAGS) -MMD"
-endif
 
 # This target generates the main/app firmware image located at 0x08010000
 $(FIRMWARE): FIRMWARE_OBJS
-	$(CPP) -P -E -DLINKER_SCRIPT -I$(OMV_BOARD_CONFIG_DIR) $(OMV_DIR)/ports/$(PORT)/$(LDSCRIPT).ld.S > $(BUILD)/$(LDSCRIPT).lds
-	$(CC) $(LDFLAGS) $(FIRM_OBJ) -o $(FW_DIR)/$(FIRMWARE).elf $(LIBS) -lgcc
+	$(CPP) -P -E -DLINKER_SCRIPT -I$(OMV_BOARD_CONFIG_DIR) \
+    $(OMV_DIR)/ports/$(PORT)/$(LDSCRIPT).ld.S > $(BUILD)/$(LDSCRIPT).lds
+	$(CC) $(LDFLAGS) $(FIRM_OBJ) -o $(FW_DIR)/$(FIRMWARE).elf $(LIBS) -lm
 	$(OBJCOPY) -Obinary -R .big_const* $(FW_DIR)/$(FIRMWARE).elf $(FW_DIR)/$(FIRMWARE).bin
-#	$(PYTHON) $(MKDFU) -D $(DFU_DEVICE) -b $(MAIN_APP_ADDR):$(FW_DIR)/$(FIRMWARE).bin $(FW_DIR)/$(FIRMWARE).dfu
 
-ifeq ($(OMV_ENABLE_UVC), 1)
-# This target generates the UVC firmware.
-$(UVC): FIRMWARE_OBJS UVC_OBJS
-	$(CPP) -P -E -I$(OMV_BOARD_CONFIG_DIR) $(UVC_DIR)/$(LD_SCRIPT).ld.S > $(BUILD)/$(UVC_DIR)/$(LD_SCRIPT).lds
-	$(CC) $(UVC_LDFLAGS) $(UVC_OBJ) -o $(FW_DIR)/$(UVC).elf -lgcc
-	$(OBJCOPY) -Obinary $(FW_DIR)/$(UVC).elf $(FW_DIR)/$(UVC).bin
-	$(PYTHON) $(MKDFU) -D $(DFU_DEVICE) -b $(MAIN_APP_ADDR):$(FW_DIR)/$(UVC).bin $(FW_DIR)/$(UVC).dfu
-endif
-
-# This target generates the uvc and firmware images.
-$(OPENMV): $(UVC) $(FIRMWARE)
+# This target generates the firmware image.
+$(OPENMV): $(FIRMWARE)
 	$(SIZE) $(FW_DIR)/$(FIRMWARE).elf
 
 size:
