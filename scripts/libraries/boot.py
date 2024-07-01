b276797c02582ba66f999bce286d0963500300e8
RICHRish <163415689+RICHRish@users.noreply.github.com>
2024-07-01 17:30:43 +0545

    Update boot.py

--- scripts/libraries/boot.py
+++ scripts/libraries/boot.py
@@ -9,10 +9,9 @@ sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time=2000)
-
+while True:
 pyb.LED(RED_LED_PIN).on()
 sensor.skip_frames(time=2000)
-pyb.LED(RED_LED_PIN).off()
 
 
 
