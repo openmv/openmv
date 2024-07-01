20121a17bd1025f3766425f9ff7711a9c8493d57
RICHRish <163415689+RICHRish@users.noreply.github.com>
2024-07-01 14:33:18 +0545

    Create boot.py

---
+++ scripts/libraries/boot.py
@@ -0,0 +1,17 @@
+import sensor, image, pyb, machine,time, sys
+
+
+RED_LED_PIN = 1
+BLUE_LED_PIN = 3
+
+# Initialize the camera
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time=2000)
+
+pyb.LED(RED_LED_PIN).on()
+sensor.skip_frames(time=2000)
+pyb.LED(RED_LED_PIN).off()
+
+
