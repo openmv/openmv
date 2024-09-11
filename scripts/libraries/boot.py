f7381367e379d4f927a9182ab9e668491adb74de
RICHRish <kritika200014@gmail.com>
2024-09-11 09:38:38 +0545

    HEAD

--- scripts/libraries/boot.py
+++ scripts/libraries/boot.py
@@ -1,12 +1,18 @@
 # Untitled - By: kriti - Tue Jul 2 2024
 
-import time, machine
-import pyb 
+import sensor, image, pyb, machine,time, sys
+
+
 
 uart = UART(1, 9600)                        
 uart.init(9600, bits=8, parity=None, stop=1) 
 RED_LED_PIN = 1
 BLUE_LED_PIN = 3
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time=2000)
 
 while(True):
     pyb.LED(BLUE_LED_PIN).on()
@@ -17,6 +23,7 @@ while(True):
     pyb.delay(1000)
     pyb.LED(RED_LED_PIN).off()
     pyb.delay(1000)
-    # uart.write("bootloader running")
+    uart.write("bootloader running")
+    sensor.skip_frames(time=2000)
 
 
