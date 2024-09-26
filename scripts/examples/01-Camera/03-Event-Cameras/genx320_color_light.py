9f0985305a74d231c2593fe605885292b5660594
Kwabena W. Agyeman <kwagyeman@live.com>
2024-09-26 13:48:42 -0700

    scripts/examples: Add Genx320 example scripts.

---
+++ scripts/examples/01-Camera/03-Event-Cameras/genx320_color_light.py
@@ -0,0 +1,24 @@
+# This work is licensed under the MIT license.
+# Copyright (c) 2013-2024 OpenMV LLC. All rights reserved.
+# https://github.com/openmv/openmv/blob/master/LICENSE
+#
+# This example shows off using the genx320 event camera from Prophesee.
+
+import sensor
+import image
+import time
+
+sensor.reset()
+sensor.set_pixformat(sensor.GRAYSCALE)  # Must always be grayscale.
+sensor.set_framesize(sensor.B320X320)  # Must always be 320x320.
+sensor.set_color_palette(image.PALETTE_EVT_LIGHT)
+
+clock = time.clock()
+
+while True:
+    clock.tick()
+
+    img = sensor.snapshot()
+    # img.median(1) # noise cleanup.
+
+    print(clock.fps())
