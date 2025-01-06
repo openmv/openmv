1ac766d56afbfdc8c5dc5a747210bdad9b251401
iabdalkader <i.abdalkader@gmail.com>
2025-01-06 10:09:45 +0100

    bootloader: Remove unused prototypes.

--- src/boot/include/port.h
+++ src/boot/include/port.h
@@ -96,8 +96,6 @@ void port_pin_mode(uint32_t pin, uint32_t mode);
 uint32_t port_pin_read(uint32_t pin);
 void port_pin_write(uint32_t pin, uint32_t state);
 void port_led_blink(uint32_t interval_ms);
-int port_flash_read(uint32_t ptype, uint32_t addr, uint8_t *buf, uint32_t size);
-int port_flash_write(uint32_t ptype, uint32_t addr, const uint8_t *buf, uint32_t size);
 void port_mpu_init(void);
 void port_mpu_deinit(void);
 void port_mpu_load_defaults();
