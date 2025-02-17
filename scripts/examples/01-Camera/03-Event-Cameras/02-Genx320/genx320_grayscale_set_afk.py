06be960574e84e40efb1d510e56446e5451ae380
Natalia Lyubova <nlyubova@prophesee.ai>
2025-02-17 17:07:08 +0100

    sensors/GenX320: Adding IOCTL to control Anti-Flicker filter (AFK).

---
+++ scripts/examples/01-Camera/03-Event-Cameras/02-Genx320/genx320_grayscale_set_afk.py
@@ -0,0 +1,29 @@
+# This work is licensed under the MIT license.
+# Copyright (c) 2013-2024 OpenMV LLC. All rights reserved.
+# https://github.com/openmv/openmv/blob/master/LICENSE
+#
+# This example shows off using the GenX320 event sensor from Prophesee and controlling
+# AntiFlicKer (AFK) filter bloc in the GenX320 digital pipeline.
+# AFK allows to detect periodic changes of given frequencies in the scene and filter them out.
+
+import sensor
+import time
+
+sensor.reset()
+sensor.set_pixformat(sensor.GRAYSCALE)  # Must always be grayscale.
+sensor.set_framesize(sensor.B320X320)  # Must always be 320x320.
+sensor.set_framerate(100)
+
+# Enables AFK filter to remove periodic data in the frequency range from 130Hz to 160Hz
+sensor.ioctl(sensor.IOCTL_GENX320_SET_AFK, 1, 130, 160)
+# Disables AFK filter
+# sensor.ioctl(sensor.IOCTL_GENX320_SET_AFK, 0)
+
+clock = time.clock()
+
+while True:
+    clock.tick()
+
+    img = sensor.snapshot()
+
+    print(clock.fps())
