30031c3ea2d91c13e9afbc71dfa6ec55abc5dfab
Kwabena W. Agyeman <kwagyeman@live.com>
2025-07-02 21:15:07 -0700

    boards: Remove old GENX20 defines.

--- boards/OPENMV_N6/omv_boardconfig.h
+++ boards/OPENMV_N6/omv_boardconfig.h
@@ -49,8 +49,6 @@
 #define OMV_PAG7936_MIPI_CSI2               (1)
 #define OMV_SOFTCSI_ENABLE                  (1)
 #define OMV_PS5520_ENABLE                   (1)
-#define OMV_GENX320_EHC_ENABLE              (1)
-#define OMV_GENX320_HSYNC_VALUE             (0x1)
 
 // FIR drivers configuration.
 #define OMV_FIR_MLX90621_ENABLE             (1)
