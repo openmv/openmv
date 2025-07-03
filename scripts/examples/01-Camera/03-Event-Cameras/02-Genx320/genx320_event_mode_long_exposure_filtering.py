eb79c9f3b3228ee1775681b44b9a16c90d52c8a0
Kwabena W. Agyeman <kwagyeman@live.com>
2025-07-02 21:54:02 -0700

    scripts/examples: Add examples for RAW event camera output.

---
+++ scripts/examples/01-Camera/03-Event-Cameras/02-Genx320/genx320_event_mode_long_exposure_filtering.py
@@ -0,0 +1,68 @@
+# This work is licensed under the MIT license.
+# Copyright (c) 2013-2025 OpenMV LLC. All rights reserved.
+# https://github.com/openmv/openmv/blob/master/LICENSE
+#
+# This example shows off using the genx320 event camera from Prophesee
+# using event streaming mode drawing multiple frames on the same image.
+#
+# Adds filtering of events based on the event type (PIX_ON_EVENT or PIX_OFF_EVENT).
+
+import csi
+import image
+import time
+# https://micropython-ulab.readthedocs.io/en/latest/index.html
+from ulab import numpy as np
+
+TARGET_EVENT_TYPE = csi.PIX_ON_EVENT  # Change to PIX_OFF_EVENT to filter low events.
+
+EXPOSURE_FRAMES = 10
+
+# Surface to draw the histogram image on.
+img = image.Image(320, 320, image.GRAYSCALE)
+
+# ndarray to hold events from the camera
+# must be 2048 events by 6 values
+#
+# 0: event type
+# 1: seconds timestamp
+# 2: milliseconds timestamp
+# 3: microseconds timestamp
+# 4: x coordinate (0-319 for the genx320)
+# 5: y coordinate (0-319 for the genx320)
+events = np.zeros((2048, 6), dtype=np.uint16)
+
+# Initialize the sensor.
+csi0 = csi.CSI(cid=csi.GENX320)
+csi0.reset()
+
+clock = time.clock()
+i = 0
+while True:
+    clock.tick()
+
+    # Reads up to 2048 events from the camera.
+    # Returns the number of valid events (0-2048) or a negative error code.
+    # Note that old events in the buffer are not cleared to save CPU time.
+    event_count = csi0.ioctl(csi.IOCTL_GENX320_READ_EVENTS, events)
+    events_slice = events[:event_count]
+    indices = np.nonzero(events_slice[:, 0] == TARGET_EVENT_TYPE)[0]
+    if len(indices):
+        target_events = np.take(events_slice, indices, axis=0)
+
+        # Clear the image every EXPOSURE_FRAMES frames.
+        c = (i % EXPOSURE_FRAMES) == 0
+        i += 1
+
+        # Draws the events on the image. If clear=True then the image is cleared
+        # to the default value of "brightness". Event histogram bins have "contrast"
+        # added to them for PIX_ON_EVENT events and subtracted from them for
+        # PIX_OFF_EVENT events clampped between 0 and 255. Pass clear=False to keep
+        # accumulating events in the histogram image.
+        img.draw_event_histogram(target_events, clear=c, brightness=128, contrast=64)
+
+        # Push the image to the jpeg buffer for the IDE to pull and display.
+        # The IDE pulls frames off the camera at a much lower rate than the
+        # onboard camera frame rate printed below.
+        img.flush()
+
+        print(len(target_events), clock.fps())
