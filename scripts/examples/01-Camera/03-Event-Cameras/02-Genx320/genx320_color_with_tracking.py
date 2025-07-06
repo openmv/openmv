7b5c32e57f762ddc9f0ef72967b2fca9beb0d3dc
Kwabena W. Agyeman <kwagyeman@live.com>
2025-07-06 16:25:00 -0700

    scripts/examples: Merge genx320 light/dark examples.

--- scripts/examples/01-Camera/03-Event-Cameras/02-Genx320/genx320_color_with_tracking.py
+++ scripts/examples/01-Camera/03-Event-Cameras/02-Genx320/genx320_color_with_tracking.py
@@ -13,7 +13,7 @@ sensor.set_pixformat(sensor.GRAYSCALE)  # Must always be grayscale.
 sensor.set_framesize(sensor.B320X320)  # Must always be 320x320.
 sensor.set_brightness(128)  # Leave at 128 generally (this is the default).
 sensor.set_contrast(16)  # Increase to make the image pop.
-sensor.set_color_palette(image.PALETTE_EVT_LIGHT)
+sensor.set_color_palette(image.PALETTE_EVT_LIGHT)  # image.PALETTE_EVT_DARK for dark mode.
 
 # The default frame rate is 50 FPS. You can change it between ~20 FPS and ~350 FPS.
 sensor.set_framerate(50)
@@ -25,5 +25,13 @@ while True:
 
     img = sensor.snapshot()
     # img.median(1) # noise cleanup.
+
+    blobs = img.find_blobs(
+        [(85, 95, -10, 10, -10, 10)], invert=True, pixels_threshold=10, area_threshold=100, merge=True
+    )
+
+    for blob in blobs:
+        img.draw_rectangle(blob.rect(), color=(255, 0, 0))
+        img.draw_cross(blob.cx(), blob.cy(), color=(0, 255, 0))
 
     print(clock.fps())
