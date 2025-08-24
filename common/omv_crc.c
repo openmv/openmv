2d32f5a8901d18a57943f42a5f1942204e0ad1d2
iabdalkader <i.abdalkader@gmail.com>
2025-08-24 19:04:44 +0200

    common: Add CRC HAL.
    Signed-off-by: iabdalkader <i.abdalkader@gmail.com>

---
+++ common/omv_crc.c
@@ -0,0 +1,47 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (C) 2025 OpenMV, LLC.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include "omv_crc.h"
+
+__attribute__((weak)) void omv_crc_init(void) {
+
+}
+
+__attribute__((weak)) uint16_t omv_crc_calc(const void *buf, size_t size) {
+    const uint8_t *data = (const uint8_t *)buf;
+    uint16_t crc = 0xFFFF;
+    
+    for (size_t i = 0; i < size; i++) {
+        crc ^= (uint16_t)data[i] << 8;
+        
+        for (int j = 0; j < 8; j++) {
+            if (crc & 0x8000) {
+                crc = (crc << 1) ^ OMV_CRC16_POLY;
+            } else {
+                crc <<= 1;
+            }
+        }
+    }
+    
+    return crc;
+}
