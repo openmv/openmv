scripts/libraries: Fix pre-processing float input array normalization.
ULAB only does simple assignment operator operations in-place.

scripts/libraries/ml/ml/preprocessing.py
@@ -93,4 +93,6 @@ class Normalization:
                 fadd = (fadd - np.array(self.mean)) / np.array(self.stdev)
                 fscale = fscale / np.array(self.stdev)
 
-            array = (array * fscale) + fadd
+            # Apply normalization in-place (must be done in two steps for ulab).
+            array *= fscale
+            array += fadd