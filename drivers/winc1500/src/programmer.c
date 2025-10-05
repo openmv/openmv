78ad9448dc781cc650eb169c28f5c22d4c68e5f8
Andrew Leech <andrew.leech@planetinnovation.com.au>
2025-10-05 21:35:11 +1100

    drivers/winc1500: Replace FatFS calls with VFS abstraction.
    Signed-off-by: Andrew Leech <andrew.leech@planetinnovation.com.au>

--- drivers/winc1500/src/programmer.c
+++ drivers/winc1500/src/programmer.c
@@ -30,11 +30,6 @@
 #include "programmer/programmer.h"
 #include "spi_flash/include/spi_flash_map.h"
 
-#define MIN(a,b) \
-    ({ __typeof__ (a) _a = (a); \
-     __typeof__ (b) _b = (b); \
-     _a < _b ? _a : _b; })
-
 /**
  * Program firmware to WINC1500 memory.
  *
@@ -42,27 +37,22 @@
  */
 int burn_firmware(const char *path)
 {
-    FIL fp;
+    file_t fp;
     uint32_t offset = 0;
-    UINT bytes = 0, bytes_out=0;
+    size_t bytes = 0;
 
     int ret = M2M_ERR_FAIL;
     uint8_t	*buf = fb_alloc(FLASH_SECTOR_SZ, FB_ALLOC_NO_HINT);
 
-    if (file_ll_open(&fp, path, FA_READ|FA_OPEN_EXISTING) != FR_OK) {
-        goto error;
-    }
+    file_open(&fp, path, false, FA_READ | FA_OPEN_EXISTING);
 
     // Firmware image size
-    uint32_t size = f_size(&fp);
+    uint32_t size = file_size(&fp);
 
     while (size) {
         // Read a chuck (max FLASH_SECTOR_SZ bytes).
         bytes = MIN(size, FLASH_SECTOR_SZ);
-        if (file_ll_read(&fp, buf, bytes, &bytes_out) != FR_OK || bytes != bytes_out) {
-            printf("burn_firmware: file read error!\n");
-            goto error;
-        }
+        file_read(&fp, buf, bytes);
 
         // Write firmware sector to the WINC1500 memory.
         if (programmer_write_firmware_image(buf, offset, bytes) != M2M_SUCCESS) {
@@ -78,7 +68,7 @@ int burn_firmware(const char *path)
 
 error:
     fb_free();
-    file_ll_close(&fp);
+    file_close(&fp);
     return ret;
 }
 
@@ -88,29 +78,24 @@ error:
  */
 int verify_firmware(const char *path)
 {
-    FIL fp;
+    file_t fp;
     uint32_t offset = 0;
-    UINT bytes = 0, bytes_out=0;
+    size_t bytes = 0;
 
     int ret = M2M_ERR_FAIL;
     uint8_t	*file_buf = fb_alloc(FLASH_SECTOR_SZ, FB_ALLOC_NO_HINT);
     uint8_t	*flash_buf = fb_alloc(FLASH_SECTOR_SZ, FB_ALLOC_NO_HINT);
 
-    if (file_ll_open(&fp, path, FA_READ|FA_OPEN_EXISTING) != FR_OK) {
-        goto error;
-    }
+    file_open(&fp, path, false, FA_READ | FA_OPEN_EXISTING);
 
     // Firmware image size
-    uint32_t size = f_size(&fp);
+    uint32_t size = file_size(&fp);
 
     while (size) {
         // Firmware chuck size (max FLASH_SECTOR_SZ bytes).
         bytes = MIN(size, FLASH_SECTOR_SZ);
 
-        if (file_ll_read(&fp, file_buf, bytes, &bytes_out) != FR_OK || bytes_out != bytes) {
-            printf("verify_firmware: file read error!\n");
-            goto error;
-        }
+        file_read(&fp, file_buf, bytes);
 
         if (programmer_read_firmware_image(flash_buf, offset, bytes) != M2M_SUCCESS) {
             printf("verify_firmware: read access failed on firmware section!\r\n");
@@ -133,7 +118,7 @@ int verify_firmware(const char *path)
 error:
     fb_free();
     fb_free();
-    file_ll_close(&fp);
+    file_close(&fp);
     return ret;
 }
 
@@ -143,16 +128,14 @@ error:
  */
 int dump_firmware(const char *path)
 {
-    FIL fp;
+    file_t fp;
     uint32_t offset = 0;
-    UINT bytes = 0, bytes_out=0;
+    size_t bytes = 0;
 
     int ret = M2M_ERR_FAIL;
     uint8_t	*flash_buf = fb_alloc(FLASH_SECTOR_SZ, FB_ALLOC_NO_HINT);
 
-    if (file_ll_open(&fp, path, FA_WRITE | FA_CREATE_ALWAYS) != FR_OK) {
-        goto error;
-    }
+    file_open(&fp, path, false, FA_WRITE | FA_CREATE_ALWAYS);
 
     // Firmware image size
     uint32_t size = FLASH_4M_TOTAL_SZ;
@@ -166,10 +149,7 @@ int dump_firmware(const char *path)
             goto error;
         }
 
-        if (file_ll_write(&fp, flash_buf, bytes, &bytes_out) != FR_OK || bytes_out != bytes) {
-            printf("dump_firmware: file write error!\n");
-            goto error;
-        }
+        file_write(&fp, flash_buf, bytes);
 
         size -= bytes;
         offset += bytes;
@@ -179,6 +159,6 @@ int dump_firmware(const char *path)
 
 error:
     fb_free();
-    file_ll_close(&fp);
+    file_close(&fp);
     return ret;
 }
