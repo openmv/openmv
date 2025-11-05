8807b0fe41d978b2c6ea045113842dd633c7f3b2
Andrew Leech <andrew.leech@planetinnovation.com.au>
2025-11-05 21:47:47 +1100

    ports/qemu: Provide malloc/calloc/free using MicroPython GC.
    QEMU's semihosting environment doesn't support traditional heap allocation
    via _sbrk, causing C code that uses calloc to fail. This implements
    malloc/calloc/realloc/free using MicroPython's garbage collector, enabling
    standard C allocation on QEMU.

--- ports/qemu/qemu_semihost_stubs.c
+++ ports/qemu/qemu_semihost_stubs.c
@@ -7,12 +7,51 @@
  * QEMU uses ARM semihosting for I/O, but libc still needs these stubs.
  */
 #include <errno.h>
+#include <string.h>
 #include <sys/stat.h>
+#include "py/gc.h"
+#include "py/runtime.h"
 
 // QEMU uses MicroPython's GC for heap allocation, not sbrk
 void *_sbrk(int incr) {
     errno = ENOMEM;
-    return (void *)-1;
+    return (void *) -1;
+}
+
+// Provide malloc/calloc/free using MicroPython's GC
+// This is needed for drivers like softcsi that use standard C allocation
+void *malloc(size_t size) {
+    void *ptr = gc_alloc(size, false);
+    return ptr;
+}
+
+void *calloc(size_t nmemb, size_t size) {
+    size_t total_size = nmemb * size;
+    void *ptr = gc_alloc(total_size, false);
+    if (ptr) {
+        memset(ptr, 0, total_size);
+    }
+    return ptr;
+}
+
+void *realloc(void *ptr, size_t size) {
+    if (ptr == NULL) {
+        return malloc(size);
+    }
+    if (size == 0) {
+        // Note: Not calling free(ptr) since GC will collect it
+        return NULL;
+    }
+    void *new_ptr = gc_realloc(ptr, size, true);
+    return new_ptr;
+}
+
+void free(void *ptr) {
+    // GC will collect unused memory automatically
+    // We can explicitly mark as free if needed
+    if (ptr) {
+        gc_free(ptr);
+    }
 }
 
 int _close(int file) {
